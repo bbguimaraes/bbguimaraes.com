06e350d4e6f0929f8d08e845fb8656e883c58b55
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2021-03-27 19:34:24 +0000

    scripts/tidy: ignore external files

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-for f in $(find bbguimaraes.com/ -type f -a -name '*.html'); do
+for f in $(find \
+    bbguimaraes.com/ \
+    -path bbguimaraes.com/files -prune -o \( -type f -a -name '*.html' \) \
+); do
     if ! out=$(tidy -quiet -errors "$f" 2>&1); then
         printf '%s\n%s\n' "$f" "$out"
     fi
