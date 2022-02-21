0ac823631262f8ad3a88fb92bb7f48e1692691f0
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2022-02-21 14:19:00 +0000

    scripts/tidy.sh: process arguments

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-for f in $(find \
-    bbguimaraes.com/ \
-    -path bbguimaraes.com/files -prune -o \( -type f -a -name '*.html' \) \
-); do
+if [[ "$#" -eq 0 ]]; then
+    set -- $(find \
+        bbguimaraes.com/ \
+        -path bbguimaraes.com/files -prune -o \( -type f -a -name '*.html' \) \
+    )
+fi
+for f; do
     if ! out=$(tidy -quiet -errors "$f" 2>&1); then
         printf '%s\n%s\n' "$f" "$out"
     fi
