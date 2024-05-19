e262161426f058c8fa6d59d77d05dd1dc7adb1f5
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-05-19 14:50:33 +0200

    places: add main gallery

--- src/places/index.lua
+++ src/places/index.lua
@@ -1,3 +1,13 @@
+local generate <const> = require "lib.generate"
+local path <const> = require "lib.path"
+local util <const> = require "lib.util"
+
+local DIR <const> = path.join("src", "places", "data")
+local FILES_URL <const> = path.join("", "files", "places")
+local FILES_DIR <const> = "bbguimaraes.com" .. FILES_URL
+
+local IMAGES <const> = {}
+
 local cit0 <const> = lines {
     par [[
 Caelum, nón animum mútant, quí tráns mare currunt.
@@ -36,6 +46,90 @@ else than the good ordering of the mind.]]):gsub("\n", " ") .. [[
 — Marcus Aurelius, Meditations
 ]]
 
+local function generate_item(_, t)
+    local id <const> = t.id
+    local l <const> = {}
+    table.insert(l, image {
+        class = "main-img",
+        alt = t.title,
+        src = path.join(FILES_URL, t.images[1].path:gsub("%.", "_small."), nil),
+    })
+    table.insert(l, div({class = "title"}, lines {
+        inline_tag("h2", nil, t.title),
+        html(t.timestamp[2]),
+    }))
+    return div(
+        {class = "place"},
+        tag("a", {href = id .. ".html"}, lines(l)))
+end
+
+local find_images
+local generate_small_image
+local function generate_images(t)
+    local id <const> = t.id:gsub("-", "_")
+    local ret <const> = t.images or find_images(id)
+    for _, x in ipairs(ret) do
+        generate_small_image(x, "_small", "512x384")
+        generate_small_image(x, "_tiny", "128x87")
+    end
+    return ret
+end
+
+function find_images(id)
+    local ret <const> = {}
+    local pat <const> = path.join("", id .. ".*%.jpg$")
+    for x in pairs(IMAGES) do
+        if not x:match(pat) then
+            goto continue
+        end
+        x = path.base(x)
+        if x:match("_small.jpg$") or x:match("_tiny.jpg$") then
+            goto continue
+        end
+        table.insert(ret, x)
+        ::continue::
+    end
+    table.sort(ret)
+    for k, v in pairs(ret) do
+        ret[k] = {path = v}
+    end
+    return ret
+end
+
+function generate_small_image(t, suffix, size)
+    local name = t.path
+    local small <const> = path.join(
+        FILES_DIR,
+        name:gsub("%.[^.]+$", suffix .. ".jpg"), nil)
+    if IMAGES[small] then
+        return
+    end
+    name = path.join(FILES_DIR, name)
+    if name:match("%.mp4$") then
+        assert(os.execute(string.format(
+            "ffmpeg "
+                .. " -loglevel warning -ss %s -i %s"
+                .. " -vf scale=%s -update 1 -vframes 1 %s",
+            t.poster or "0:00", name, size, small)))
+    else
+        assert(os.execute(string.format(
+            "convert -resize '%s>' %s %s", size, name, small)))
+    end
+end
+
+for x in path.each(path.join(FILES_DIR, "*.jpg")) do
+    IMAGES[x] = true
+end
+
+local files <const> = {}
+for x in path.each(DIR) do
+    local t <const> = generate.load(path.join(DIR, x))
+    local id <const> = t.id
+    t.images = generate_images(t)
+    table.insert(files, t)
+end
+table.sort(files, function(x, y) return y.timestamp[1] < x.timestamp[1] end)
+
 return include "master.lua" {
     title = "places",
     css = {"/main.css", "places.css"},
@@ -61,6 +155,11 @@ return include "master.lua" {
                     "— Marcus Aurelius,",
                     inline_tag("span", {lang = "grc"}, "Τὰ εἰς ἑαυτόν"),
                 }),
+            }),
+        }),
+        main(nil, lines {
+            div({class = "places gallery"}, lines {
+                table.unpack(util.imap(generate_item, files))
             }),
         }),
     },
