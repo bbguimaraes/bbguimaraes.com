29f001c40c60a7875c0f7201a0175f4a056dd879
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-05-27 19:27:35 +0200

    Add roman font and white background options

---
+++ bbguimaraes.com/main.js
@@ -0,0 +1,60 @@
+let nav = document.querySelector("nav");
+if(nav) {
+    let make_listener = (key, cls, sel) => {
+        let l = document.body.classList;
+        let reset = l.contains(cls) ? l.add : l.remove;
+        return _ => {
+            let i = sel.selectedIndex;
+            if(i) {
+                localStorage[key] = sel.options[i].value;
+                switch(i) {
+                case 1: l.remove(cls); break;
+                case 2: l.add(cls); break;
+                }
+            } else {
+                localStorage.removeItem(key);
+                reset.call(l, cls);
+            }
+        };
+    };
+    let make_sel = (title, key, cls, values) => {
+        let [v0, v1, v2] = values;
+        let ret = document.createElement("select");
+        ret.required = true;
+        ret.innerHTML = `
+<option value="">${v0}</option>
+<option>${v1}</option>
+<option>${v2}</option>
+`;
+        ret.title = title;
+        ret.addEventListener("change", make_listener(key, cls, ret));
+        return ret;
+    };
+    let set_initial = (key, sel) => {
+        let v = localStorage[key];
+        if(!v)
+            return;
+        let o = Array.prototype.find.call(
+            sel.querySelectorAll("option"),
+            x => x.value == v);
+        if(!o)
+            return;
+        sel.selectedIndex = o.index;
+        sel.dispatchEvent(new Event("change"));
+    };
+    let create = (div, title, key, cls, values) => {
+        let sel = make_sel(title, key, cls, values);
+        div.appendChild(sel);
+        set_initial(key, sel);
+    };
+    let div = document.createElement("div");
+    div.classList.add("nav-opt");
+    create(div, "font style", "font", "roman", [
+        "font", "mono", "roman",
+    ]);
+    create(div, "background color", "bg_color", "white-bg", [
+        "color", "black", "white",
+    ]);
+    div.appendChild(document.querySelector("#rss-icon"));
+    nav.appendChild(div);
+}
