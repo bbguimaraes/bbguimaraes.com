f578b665c7ced3328d1d98b9c6c6a82cf15f9c64
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-06-20 11:28:33 +0200

    music: wayfaring stranger

---
+++ src/music/data/wayfaring-stranger.lua
@@ -0,0 +1,49 @@
+local content <const> = lines {
+    par [[
+Old folk/gospel songs have always been some of my favorites, this one in
+particular. The lyrics and the atmosphere are just perfect, and it was made to
+be played on a Telecaster.
+]],
+    par [[
+Having accidentally found this creek (note: not the River Jordan) in Parco del
+Nobile while wandering in the hills in the countryside of Torino, I decided to
+record a little version right then and there, even though I'd just learned to
+play the song earlier that day (as you can tell).
+]],
+    par [[
+This stream is small but mighty, so much so you can hear the water through the
+pick-ups of the guitar, so there is quite a bit of background noise. But at
+least you get to hear what it actually sounded like (as opposed to a sanitized
+studio sound) spending the afternoon in this fantastic place. There is nothing
+in this world like playing music out in nature. At some point while I was
+recording, I even got to make the soundtrack for a bird bathing in the river.
+]],
+    par [[
+Sound recorded with the amazing little VOX amPlug 2 portable guitar amplifier
+and my always-faithful ZOOM H2n.
+]],
+    blockquote(lines {
+    par [[
+I'm just a poor wayfaring stranger<br />
+Traveling through this world of woe<br />
+Yet there's no sickness, toil, or danger<br />
+In this land to which I go
+]],
+    par [[
+I'm going there to see my father<br />
+I'm going there no more to roam<br />
+I'm just going over Jordan<br />
+I'm just going over home
+]],
+    }),
+}
+
+return {
+    id = "wayfaring-stranger",
+    title = "Wayfaring stranger",
+    date = { "1718802939", "2024-06-19T15:15:39" },
+    duration = "2:23",
+    tags = {"guitar", "vocals", "short"},
+    youtube = "0d1R_8d6sqI",
+    content = content,
+}
