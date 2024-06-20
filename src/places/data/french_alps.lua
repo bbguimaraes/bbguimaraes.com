d0379d8fecbaf76ed730d48f579d1eeddf00336e
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-06-20 18:47:31 +0200

    wip alps

---
+++ src/places/data/french_alps.lua
@@ -0,0 +1,24 @@
+local images <const> = {{
+    path = "french_alps0.jpg",
+}, {
+    path = "french_alps1.jpg",
+}, {
+    path = "french_alps2.jpg",
+}, {
+    path = "french_alps3.jpg",
+}}
+
+return {
+    id = "french-alps",
+    title = "French Alps",
+    timestamp = {"1703203200", "2023-12-22"},
+    description = description,
+    citation = citation,
+    images = images,
+--    links = {
+--        map = "",
+--        mastodon = "",
+--        facebook = "",
+--        instagram = "",
+--    },
+}
