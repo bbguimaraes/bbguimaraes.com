29dcf38a34e46551868c2d3a192fab5b4e020ceb
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-06-27 16:48:55 +0200

    scripts/date: accept date as optional argument

--- scripts/date.sh
+++ scripts/date.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-t=$(date +%s)
+t=${1+$(TZ=UTC date +%s --date "$1")}
 d=$(TZ=UTC date --date "@$t" +%Y-%m-%dT%H:%M:%S)
 printf 'date = {"%s", "%s"}\n' "$t" "$d"
