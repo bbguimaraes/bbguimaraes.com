88ec48c3e99a2c6686bd6f24b0efb54ac767245e
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-09-20 13:33:16 +0200

    src: make OpenGraph image tag optional

--- src/include/og.lua
+++ src/include/og.lua
@@ -1,10 +1,10 @@
 local t <const> = {}
 
-for _, x in ipairs{"type", "title", "image", "url"} do
+for _, x in ipairs{"type", "title", "url"} do
     table.insert(t, property("og:" .. x, var(x)))
 end
 
-for _, x in ipairs{"description", "video"} do
+for _, x in ipairs{"description", "image", "video"} do
     var_and(x, function(v)
         table.insert(t, property("og:" .. x, v))
     end)
