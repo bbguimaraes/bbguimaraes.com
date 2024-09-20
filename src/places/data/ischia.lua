b03d90e0ab140959213c5136180fe5ca980131df
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-09-20 13:37:46 +0200

    places: Ischia

---
+++ src/places/data/ischia.lua
@@ -0,0 +1,223 @@
+local description <const> = [[The last stop of my marathon in the Gulf of Napoli.  I had become so enamored of the island from watching it all the way across the Gulf of Gaeta, more than 60km away — it is located right between the two gulfs, in the area where Campi Flegrei, the huge volcanic caldera, enters the Mediterranean sea, forming Penisola flegrea and the surrounding islands — seeing its dim figure through the maritime mist, its shaded contour against the colored sky at sunset, its dark green suspended in the bright blue where both sky and sea meet on sunny days (<a href="https://bbguimaraes.com/places/coreno-ausonio.html">see</a> <a href="https://bbguimaraes.com/places/castelforte.html">the</a> <a href="https://bbguimaraes.com/places/santi-cosma-e-damiano.html">other</a> <a href="https://bbguimaraes.com/places/gaeta.html">posts</a> <a href="https://bbguimaraes.com/places/napoli.html">where</a> <a href="https://bbguimaraes.com/places/capri.html">it</a> <a href="https://bbguimaraes.com/places/monte-solaro.html">makes</a> <a href="https://bbguimaraes.com/places/sorrento.html">an</a> <a href="https://bbguimaraes.com/places/herculaneum.html">appearance</a>), that I could not leave Campania without going there.]]
+
+local content <const> = lines {
+    par [[
+The last stop of my marathon in the Gulf of Napoli.  I had become so enamored of
+the island from watching it all the way across the Gulf of Gaeta, more than 60km
+away — it is located right between the two gulfs, in the area where Campi
+Flegrei, the huge volcanic caldera, enters the Mediterranean sea, forming
+Penisola flegrea and the surrounding islands — seeing its dim figure through the
+maritime mist, its shaded contour against the colored sky at sunset, its dark
+green suspended in the bright blue where both sky and sea meet on sunny days
+(<a href="coreno-ausonio.html">see</a> <a href="castelforte.html">the</a>
+<a href="santi-cosma-e-damiano.html">other</a> <a href="gaeta.html">posts</a>
+<a href="napoli.html">where</a> <a href="capri.html">it</a>
+<a href="monte-solaro.html">makes</a> <a href="sorrento.html">an</a>
+<a href="herculaneum.html">appearance</a>), that I could not leave Campania
+without going there.
+]],
+    par [[
+Napoli, the <a href="napoli.html" lang="grc">Νεάπολις</a>, and the surrounding
+area was the center of <span lang="la">Magna Graecia</span>, the Greek colonies
+in southern Italy, and the settlement of Ischia goes back to the 9th century
+B.C. as a trading post in the Mediterranean.  Its archaeological museum is home
+to one of the earliest inscriptions in the Greek language ever found, a cup made
+in an Aegean island in the 8th century B.C., discovered just 70 years ago in a
+tomb in the island.
+]],
+    par [[
+The trip from Napoli starts in
+<a href="https://www.openstreetmap.org/node/4822113914">Porta di Massa</a>, the
+port at the center of the city, from which also depart boats to
+<a href="capri.html">Capri</a> and other destinations in and around the gulf.
+It takes about 90 minutes for the ferry to get there (the faster hydrofoil takes
+just over an hour), but that is hardly noticeable, since there isn't a single
+minute along the <a href="https://www.openstreetmap.org/way/501455083">path</a>
+without beautiful views of the coast, <a href="napoli.html">the city of
+Napoli</a>, the
+<a href="https://www.openstreetmap.org/way/437376371">island of Nisida</a>,
+<a href="https://www.openstreetmap.org/node/11828883740">Capo di Posillipo</a>,
+the <a href="https://www.openstreetmap.org/relation/10723501">island of
+Procida</a>, etc., as well as those across the gulf:
+<a href="vesuvius.html">Vesuvius</a>, <a href="capri.html">Capri</a>, and the
+<a href="sorrento.html">Sorrento</a> peninsula.
+]],
+    par [[
+There was no chance I could explore the entire island (it is the largest in the
+region, almost 50km²), since I had only a single day to visit it, so I just
+decided to explore without planning much in advance (but that also just means
+I'll have to return =).
+]],
+    par [[
+The day started very sunny, and the deep azure typical of the waters of the Gulf
+was displayed in its full glory all along the northeastern coast, from Punta San
+Pietro all the way to the castle.  After a stop for lunch and a bus trip to the
+village of Fontana, the weather turned very misty as I made my way to
+<a href="https://www.openstreetmap.org/node/26863248">monte Epomeo</a>, the
+highest point of the island (788m) and, by the time I got to the summit, it was
+almost completely surrounded by clouds and the shore, just a few kilometers
+away, was barely visible.
+]],
+    par [[
+Climbing down the mount straight towards the south leads to the thermal waters
+of Sant'Angelo, where the high sulphur content of the volcanic water springs
+creates a natural swimming pool spanning the entire coast.  As the sun was
+starting to set, all I had time left for was another bus ride to the port to
+take the ferry back to Napoli, where the day ended with some pasta, seafood, and
+vino alla spina (naturally) in <a href="https://www.isaporidiparthenope.it">I
+Sapori di Parthenope</a>, a great local restaurant next to the central station I
+had to visit a second time.
+]],
+}
+
+local citation <const> = lines {
+    text_tag("p", {lang = "grc"}, [[
+ὀρθὸν οὖν εἶναι χρή, οὐχὶ ὀρθούμενον.
+]]),
+    par [[
+A man then must stand erect, not be kept erect by others.
+]],
+    text_tag("p", {lang = "grc"}, [[
+Ὀρθός, μὴ ὀρθούμενος.
+]]),
+    par [[
+Be thou erect, or be made erect.
+]],
+    quote_footer("Marcus Aurelius", "Meditations", "III.5, VII.12"),
+}
+
+local images <const> = {{
+    path = "ischia0.jpg",
+    text = [[
+Castello Aragonese, originally built in 474 B.C. (that's right, it's <em>almost
+exactly 2500 years old</em>).
+]],
+}, {
+    path = "ischia1.jpg",
+    text = [[
+The ferry departing from Porta di Massa.
+]],
+}, {
+    path = "ischia2.jpg",
+    text = [[
+From left to right: Ischia, Nisida, Capo di Posilipo.
+]],
+}, {
+    path = "ischia3.jpg",
+    text = [[
+Leaving Vesuvius behind.
+]],
+}, {
+    path = "ischia4.jpg",
+    text = [[
+Pozzuoli seen in the distance between Monte di Procida and Capo di Posilipo.
+]],
+}, {
+    path = "ischia5.jpg",
+    text = [[
+The tip of the peninsula and Vesuvius.
+]],
+}, {
+    path = "ischia6.jpg",
+    text = [[
+The port of Procida.
+]],
+}, {
+    path = "ischia7.jpg",
+    text = [[
+Vesuvius from the northeastern shore of Ischia.
+]],
+}, {
+    path = "ischia8.jpg",
+    text = [[
+The sea around the island.
+]],
+}, {
+    path = "ischia9.jpg",
+    text = [[
+Castello Aragonese from across the bridge.
+]],
+}, {
+    path = "ischia10.jpg",
+    text = [[
+Zingara ischitana (the gypsy sandwich), a local dish, at BisBoccia Bistro, next
+to the castle.
+]],
+}, {
+    path = "ischia11.jpg",
+    text = [[
+The type of place I get myself into when I'm exploring.
+]],
+}, {
+    path = "ischia12.jpg",
+    text = [[
+The peak of monte Epomeo.
+]],
+}, {
+    path = "ischia13.jpg",
+    text = [[
+Lido di San Montano and Marina di Lacco Ameno from the highest point of the
+island.
+]],
+}, {
+    path = "ischia14.jpg",
+    text = [[
+The peak is formed by white lava rocks such as these.
+]],
+}, {
+    path = "ischia15.jpg",
+    text = [[
+Vesuvius floating in the mist.
+]],
+}, {
+    path = "ischia16.jpg",
+    text = [[
+The port of Forio.
+]],
+}, {
+    path = "ischia17.jpg",
+    text = [[
+Stone gate on the way to Pietra dell'Acqua.
+]],
+}, {
+    path = "ischia18.jpg",
+    text = [[
+The way down to Sant'Angelo.
+]],
+}, {
+    path = "ischia19.jpg",
+    text = [[
+Clear distinction between sea water and volcanic springs.
+]],
+}, {
+    path = "ischia20.jpg",
+    text = [[
+The natural pool of Sant'Angelo.
+]],
+}, {
+    path = "ischia21.jpg",
+    text = [[
+Making my way back to the port at night.
+]],
+}, {
+    path = "ischia22.jpg",
+    text = [[
+Recovering back in Napoli with some spaghetti alle vongole.
+]],
+}}
+
+return {
+    id = "ischia",
+    title = "Ischia",
+    timestamp = {"1708992000", "2024-02-27"},
+    description = description,
+    content = content,
+    citation = citation,
+    images = images,
+    links = {
+        map = "relation/2675420",
+        mastodon = "113340532851910308",
+        facebook = "bruno.barcarolguimaraes/posts/pfbid0a5B9rrQMVcsU5x1FBhg81M2J97vefiSD1wzYLjiGJNfXoLttN4q7Q2JBBxxM4wC5l",
+        instagram = "p/DBWfOr6KxtA/",
+    },
+}
