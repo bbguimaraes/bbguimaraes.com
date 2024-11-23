a0cfa2217a3e36bc0c0104245ebc3fa3dbc0e929
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-11-23 20:26:02 +0100

    places: Formia

---
+++ src/places/data/formia.lua
@@ -0,0 +1,216 @@
+local description <const> = [[As many of the cities along the Tyrrhenian coast on the ancient via Appia, Formia was a favored place for Roman seaside villae — including one belonging to the stepfather of the emperor Augustus.  Its Greek name <span lang="grc">Όρμιαι</span> (<span lang="la">Hormiae</span> in Latin, from a word for “harbor”) refers to its many ports and the mild maritime climate provided by its surroundings: the gulf of Gaeta and the monti Aurunci.  The remains of many of these villae and ports can still be seen all they from the city center to the rocky shore on the way to <a href="https://www.openstreetmap.org/node/8390626221">monte Scauri</a>, with salient pieces of two-thousand-year-old mosaics coming out of the rock every now and then.]]
+
+local content <const> = lines {
+    par [[
+As many of the cities along the Tyrrhenian coast on the ancient via Appia,
+Formia was a favored place for Roman seaside villae — including one belonging to
+the stepfather of the emperor Augustus.  Its Greek name <span
+lang="grc">Όρμιαι</span> (<span lang="la">Hormiae</span> in Latin, from a word
+for “harbor”) refers to its many ports and the mild maritime climate provided by
+its surroundings: the gulf of Gaeta and the monti Aurunci.  The remains of many
+of these villae and ports can still be seen all they from the city center to the
+rocky shore on the way to <a
+href="https://www.openstreetmap.org/node/8390626221">monte Scauri</a>, with
+salient pieces of two-thousand-year-old mosaics coming out of the rock every now
+and then.
+]],
+    par [[
+One of the most famous residents of Formiae was the illustrious <i
+lang="la">novus homo</i> <a href="https://en.wikipedia.org/wiki/Cicero"
+lang="la">Marcus Tulius Cicero</a>.  It was there, on via Appia in 43 A.D., that
+he was assassinated by order of Marcus Antoninus, after criticizing both him and
+Julius Caesar throughout the decadent period which led to the Empire and
+admonishing Romans to return to the old Republican values — one of his most
+famous phrases, <span lang="la">“O tempora, o mores”</span>, “O times!  O
+customs!”, comes from this period.
+]],
+    par [[
+For both his implacable resistance and eminence as a public citizen, Cicero
+received the harshest punishment of all opponents of Antoninus: his severed head
+and his hands were displayed in the Roman Forum as strong discouragement of
+dissent.  The <a href="https://www.openstreetmap.org/way/159656918">monument</a>
+housing his tomb lies on a stretch of the via Appia just outside of Formia now
+named after him: <a href="https://www.openstreetmap.org/way/48503665"
+lang="it">via Marco Tulio Cicerone</a>.
+]],
+}
+
+local citation <const> = lines {
+    text_tag("p", {lang = "la"}, [[
+Imitatus est homo Romanus et consularis veterem illum Socratem, qui, cum omnium
+sapientissimus esset sanctissimeque vixisset, ita in iudicio capitis pro se ipse
+dixit, ut non supplex aut reus, sed magister, aut dominus videretur esse
+iudicum.
+]]),
+    par [[
+The Roman consul imitated Socrates of old, who, being the wisest and having
+lived the most saintly life, spoke for himself while on trial for his life as to
+be seen not supplicant as a defendant, but great as a master of the judges.
+]],
+    quote_footer(
+        "Marcus Tulius Cicero",
+        "De oratore",
+        link {
+            href = "https://www.perseus.tufts.edu/hopper/text?doc=Perseus%3Atext%3A1999.02.0120%3Abook%3D1%3Asection%3D231",
+            content = "I LIV 231",
+        }),
+}
+
+local images <const> = {{
+    path = "formia0.jpg",
+    text = [[
+Sunset in Gaeta seen from the ruins of a Roman port.
+]],
+}, {
+    path = "formia1.jpg",
+    text = [[
+Monti Aurunci and the sea in Minturno.
+]],
+}, {
+    path = "formia2.jpg",
+    text = [[
+Monte Argento (right) and Monte Orlando (left).
+]],
+}, {
+    path = "formia3.jpg",
+}, {
+    path = "formia4.jpg",
+}, {
+    path = "formia5.jpg",
+    text = [[
+The monumental tomb of Cicero (under heavy restoration that day).
+]],
+}, {
+    path = "formia6.jpg",
+}, {
+    path = "formia7.jpg",
+    text = [[
+As many of the locations in via Appia, Formia is on the path of
+<a href="francigena.html">via Francigena</a>.
+]],
+}, {
+    path = "formia8.jpg",
+    text = [[
+<span lang="it">Città di Cicerone</span>.
+]],
+}, {
+    path = "formia9.jpg",
+    text = [[
+Street art by Apolo Torres.
+]],
+}, {
+    path = "formia10.jpg",
+}, {
+    path = "formia11.jpg",
+    text = [[
+<a href="https://www.formiae.it/siti/cisternone-romano/" lang="it">Cisternone
+romano</a>, one of the largest cisterns in the world, from the 1st century B.C.
+]],
+}, {
+    path = "formia12.jpg",
+    text = [[
+Old city center around the cistern.
+]],
+}, {
+    path = "formia13.jpg",
+}, {
+    path = "formia14.jpg",
+    text = [[
+<a href="https://www.openstreetmap.org/way/1160023521">Torre di Castellone</a>.
+]],
+}, {
+    path = "formia15.jpg",
+}, {
+    path = "formia16.jpg",
+    text = [[
+Via Francigena where it reaches the coast.
+]],
+}, {
+    path = "formia17.jpg",
+    text = [[
+Criptoportici right under
+<a href="https://www.openstreetmap.org/relation/9192510" lang="it">Villa
+Comunale Umberto I</a>, a park in the city center.
+]],
+}, {
+    path = "formia18.jpg",
+}, {
+    path = "formia19.jpg",
+    text = [[
+Remains of the Roman port next to the portici.
+]],
+}, {
+    path = "formia20.jpg",
+    text = [[
+<a href="https://www.openstreetmap.org/node/8390626570">Villa Pellegrini</a>.
+]],
+}, {
+    path = "formia21.jpg",
+    text = [[
+The gulf of Gaeta from the Roman ruins of
+<a href="https://www.openstreetmap.org/way/151095052">Cisterna Maggiore</a>.
+]],
+}, {
+    path = "formia22.jpg",
+    text = [[
+The villa from the other side.
+]],
+}, {
+    path = "formia23.jpg",
+    text = [[
+Gaeta from the ruins of a Roman port.
+]],
+}, {
+    path = "formia24.jpg",
+    text = [[
+I'll stop at nothing for a picture.
+]],
+}, {
+    path = "formia25.jpg",
+}, {
+    path = "formia26.jpg",
+}, {
+    path = "formia27.jpg",
+    text = [[
+Monti Aurunci from the port.
+]],
+}, {
+    path = "formia28.jpg",
+    text = [[
+One of the mosaics buried under the rocks.
+]],
+}, {
+    path = "formia29.jpg",
+}, {
+    path = "formia30.jpg",
+    text = [[
+The sun begins to set in the gulf.
+]],
+}, {
+    path = "formia31.jpg",
+}, {
+    path = "formia32.jpg",
+}, {
+    path = "formia33.jpg",
+    text = [[
+Sunset behind the promontory of Gaeta.
+]],
+}, {
+    path = "formia34.mp4",
+}}
+
+return {
+    id = "formia",
+    title = "Formia",
+    timestamp = {"1709164800", "2024-02-29"},
+    description = description,
+    content = content,
+    citation = citation,
+    images = images,
+    links = {
+        map = "relation/41192",
+        mastodon = "113534704851590417",
+        facebook = "bruno.barcarolguimaraes/posts/pfbid02f6TUujAeoaWUZnNtspueitGmSAn6ic1Eo6gQdZbUQyq8mTVooA3XfA7XRRXkGZf4l",
+        instagram = "DCuzDVuo-Iy",
+    },
+}
