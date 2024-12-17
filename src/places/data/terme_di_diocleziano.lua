84b8bcb157830417d367f66560814cff2943c960
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2024-12-17 14:14:40 +0100

    places: Terme di Diocleziano

---
+++ src/places/data/terme_di_diocleziano.lua
@@ -0,0 +1,319 @@
+local description <const> = [[I could spend the rest of my life in Rome: every time I am there I end up spending hours in a single, small part of it, not seeing nearly enough, and leaving thinking I will have to return at least once more.  This particular time, it was this monumental complex of public baths, built at the turn of the IV century.  Located in the northeastern part of the city center, right next to the main train station, Roma Termini, it can be easily visited even during a short stay, as mine was.]]
+
+local content <const> = lines {
+    par [[
+I could spend the rest of my life in Rome: every time I am there I end up
+spending hours in a single, small part of it, not seeing nearly enough, and
+leaving thinking I will have to return at least once more.  This particular
+time, it was this monumental complex of public baths, built at the turn of the
+IV century.  Located in the northeastern part of the city center, right next to
+the main train station, Roma Termini, it can be easily visited even during a
+short stay, as mine was.
+]],
+    par [[
+Built around the time of the emperor Diocletianus, they were the largest public
+baths of the empire, with a maximum capacity of 3000 people and an area of
+around 32 acres.  After falling in disuse as a result of the interruption by the
+Goths of the large aqueduct which was their source of water, following the fall
+of the empire, the area was restored over centuries and has served many
+different purposes; to name a few: it was the birthplace of Museo Nazionale
+Romano, houses the basilica of Santa Maria degli Angeli (which, together with
+the inner cloister, was designed by Michelangelo in the XVI century), and
+supplies water for Fontana delle Naiadi in Piazza della Repubblica.
+]],
+    par [[
+Other than the remains themselves, the area around Michelangelo's cloister has
+an exhibition on the early development of civilization in the Latium (the region
+which gives both the Latin language and the modern region of Lazio their name),
+starting from the Bronze Age, millennia before Rome, the Republic, and the
+Empire; right next to it the epigraphic section has historical artefacts of
+Greek and Roman writing; finally, there are archaeological finds from the baths
+themselves as well as other areas of the city.
+]],
+    html "<hr />",
+    par [[
+The main reason for my visit was to see a few famous items housed there (at
+least “famous” for those like me).  First, a piece of pottery containing the
+earliest evidence of writing found in Italy, from the VIII century B.C. (the
+time of Homer's <i>Iliad</i> and <i>Odyssey</i>).  Its Greek inscription is
+among the three oldest ever found, together with the ones found in <a
+href="ischia.html">Ischia</a> and Athens.
+]],
+    par [[
+Then, there is a stone tablet, a funerary inscription, containing one of the
+oldest Christian writings, from the III century: <span lang="grc">“ιχθυς
+ζωντων“</span>, the “fish of the living”, which has been used as a symbol since
+the earliest period of Christianity (sometimes covertly) as, beyond the
+evangelical symbolism, its letters form the initials of the phrase:
+]],
+    blockquote [[
+<span lang="grc">Ἰησοῦς Χρῑστός Θεοῦ Υἱός Σωτήρ</span><br />
+Jesus Christ, Son of God, Savior
+]],
+    par [[
+Lastly, the object of my greatest interest: a mosaic found in a tomb in via
+Appia with the Roman Greek inscription: <span lang="grc">“Γνωθι σαυτον“</span>.
+This is the most famous of the <i>Delphic maxims</i>, a set of three phrases
+written on the temple of Apollo in Delphi, home of the Pythian prophetess:
+]],
+    blockquote [[
+<span lang="grc">Γνῶθι σεαυτόν</span>
+– know thyself
+<br />
+<span lang="grc">Μηδὲν ἄγαν</span>
+– nothing in excess
+<br />
+<span lang="grc">Ἐγγύα πάρα δ' Ἄτα</span>
+– give a pledge and trouble is at hand
+]],
+    par [[
+It was Socrates (who, as told by Plato and Xenophon, was named the wisest man by
+that very same priestess at the temple of Delphi) the first to give it its
+fullest meaning:
+]],
+    blockquote(lines {
+        text_tag("p", {lang = "grc"}, [[
+οὐ δύναμαί πω κατὰ τὸ Δελφικὸν γράμμα γνῶναι ἐμαυτόν: γελοῖον δή μοι φαίνεται
+τοῦτο ἔτι ἀγνοοῦντα τὰ ἀλλότρια σκοπεῖν.  ὅθεν δὴ χαίρειν ἐάσας ταῦτα,
+πειθόμενος δὲ τῷ νομιζομένῳ περὶ αὐτῶν, ὃ νυνδὴ ἔλεγον, σκοπῶ οὐ ταῦτα ἀλλ᾽
+ἐμαυτόν, εἴτε τι θηρίον ὂν τυγχάνω Τυφῶνος πολυπλοκώτερον καὶ μᾶλλον
+ἐπιτεθυμμένον, εἴτε ἡμερώτερόν τε καὶ ἁπλούστερον ζῷον, θείας τινὸς καὶ ἀτύφου
+μοίρας φύσει μετέχον.
+]]),
+        par [[
+I am not yet able, as the Delphic inscription has it, to know myself; so it
+seems to me ridiculous, when I do not yet know that, to investigate irrelevant
+things. And so I dismiss these matters and accepting the customary belief about
+them, as I was saying just now, I investigate not these things, but myself, to
+know whether I am a monster more complicated and more furious than Typhon or a
+gentler and simpler creature, to whom a divine and quiet lot is given by nature.
+]],
+        quote_footer(
+            "Plato",
+            "Phaedrus",
+            link {
+                href = "https://www.perseus.tufts.edu/hopper/text?doc=Perseus%3Atext%3A1999.01.0174%3Atext%3DPhaedrus%3Asection%3D229e",
+                content = "229e–230a",
+            }),
+    }),
+    par [[
+Appropriately, it appears in Petrarca's recollection of the time he spent in
+these very same ruins:
+]],
+    blockquote(lines {
+        text_tag("p", {lang = "la"}, [[
+Quis enim dubitare potest quin illico surrectura sit, si ceperit se Roma
+cognoscere?
+]]),
+        par [[
+For who can doubt that there would be an immediate resurrection, if Rome decided
+to know itself?
+]],
+    quote_footer(
+        '<span lang="it">Francesco Petrarca</span>',
+        '<span lang="la">Epistolae familiares</span>',
+        link {
+            href = "https://www.bibliotecaitaliana.it/testo/bibit000255#index.xml-body.1_div.6_div.2",
+            content = "VI.2",
+        }),
+    }),
+    par [[
+But it is the magnificent <i>De Legibus</i> of Cicero which perfectly
+synthesizes the unparalleled impact of this terse two-word precept on the
+millennia of philosophical debate since its writing:
+]],
+    blockquote(lines {
+        text_tag("p", {lang = "la"}, [[
+Est huius uero disputationis, Quinte, proprium, id quod expectas, atque utinam
+esset etiam facultatis meae!  Sed profecto ita se res habet, ut quoniam uitiorum
+emendatricem legem esse oportet commendatricemque uirtutum, ab eadem uiuendi
+doctrina ducatur.  Ita fit ut mater omnium bonarum rerum sit sapientia, a quoius
+amore Graeco uerbo philosophia nomen inuenit, qua nihil a dis immortalibus
+uberius, nihil florentius, nihil praestabilius hominum uitae datum est.  Haec
+enim una nos cum ceteras res omnes, tum, quod est difficillimum, docuit, ut
+nosmet ipsos nosceremus, cuius praecepti tanta uis et tanta sententia est, ut ea
+non homini quoipiam, sed Delphico deo tribueretur.
+]]),
+        par [[
+And indeed what you expect, my Quintus, harmonizes very well with the subjects
+of our present discussion.  And I wish that it were within my abilities to do
+justice to it.  But the real state of the case is, that since law ought to be
+both a correctress of vice and a recommender of virtue, the principles on which
+we direct our conduct ought to be drawn from her.  And, thus it comes to pass
+wisdom is the mother of all the virtuous arts, from the love of which the Greeks
+have composed the word Philosophy; and which is beyond all contradiction the
+richest, the brightest, and the most excellent of the gifts which the Gods have
+bestowed on the life of mankind.  For wisdom alone has taught us, among other
+things, the most difficult of all lessons, namely, to <i>know ourselves</i>, a
+precept so forcible and so comprehensive, that it has been attributed not to a
+man, but to the God of Delphi himself.
+]],
+    quote_footer(
+        "Marcus Tullius Cicero",
+        "De Legibus",
+        link {
+            href = "https://www.perseus.tufts.edu/hopper/text?doc=Cic.+Leg.+1&amp;fromdoc=Perseus%3Atext%3A2007.01.0030",
+            content = "I.XXI.58",
+        }),
+    }),
+}
+
+local citation <const> = lines {
+    text_tag("p", {lang = "la"}, [[
+Deambulabamus Rome soli.  Meum quidem obambulandi perypateticum morem nosti.
+Placet; nature moribusque meis aptissimus est[…].  Solebamus ergo, post
+fatigationem quam nobis immensa urbs ambita pepererat, sepius ad Termas
+Dioclitianas subsistere, nonnunquam vero supra testudinem illius
+magnificentissime olim domus ascendere, quod et aer salutaris et prospectus
+liber et silentium ac votiva solitudo nusquam magis.  […] Et euntibus per menia
+fracte urbis et illic sedentibus, ruinarum fragmenta sub oculis erant.  […] Ibi
+enim [in solitudinem], non alibi, meus sum
+]]),
+    par [[
+We walked through Rome alone.  Indeed, you know my peripatetic obambulatory
+habit.  It pleases me; it is most apt to my nature and manners[…].  We were
+wont, therefore, after the fatigue which the immense ambit of the city had
+caused us, to stop often at the Baths of Diocletian, and sometimes even to climb
+above the arch of that once magnificent house, where the salutary air, the
+boundless prospect, the silence, and the votive solitude, were never greater.
+[…] And going through the shattered city and sitting there, the fragments of the
+ruins were under our gaze.  […] Truly there [in solitude], and not elsewhere, I
+belong to myself
+]],
+    quote_footer(
+        '<span lang="it">Francesco Petrarca</span>',
+        '<span lang="la">Epistolae familiares</span>',
+        link {
+            href = "http://www.bibliotecaitaliana.it/testo/bibit000255#index.xml-body.1_div.6_div.2",
+            content = "VI.2",
+        }),
+}
+
+local images <const> = {{
+    path = "diocleziano0.jpg",
+    text = [[
+Chiostro di Michelangelo.
+]],
+}, {
+    path = "diocleziano1.jpg",
+    text = [[
+Entrance from Viale Enrico De Nicola and Piazza del Cinquecento.
+]],
+}, {
+    path = "diocleziano2.jpg",
+    text = [[
+Relief map showing the peoples of the ancient Latium and the Italian peninsula.
+]],
+}, {
+    path = "diocleziano3.jpg",
+    text = [[
+The oldest inscription found in Italy.
+]],
+}, {
+    path = "diocleziano4.jpg",
+    text = [[
+ΙΧΘΥϹ ΖΩΝΤΩΝ
+]],
+}, {
+    path = "diocleziano5.jpg",
+    text = [[
+Chiostro, with the museum sections behind it.
+]],
+}, {
+    path = "diocleziano6.jpg",
+    text = [[
+Always good to find old friends<br />
+— bust of Marcus Aurelius.
+]],
+}, { path = "diocleziano7.jpg",
+    text = [[
+Some of the best preserved sections of the original bath walls.
+]],
+}, {
+    path = "diocleziano8.jpg",
+    text = [[
+Remains of the <span lang="la">natatio</span>, the monumental swimming pool,
+with over 4000m².
+]],
+}, {
+    path = "diocleziano9.jpg",
+}, {
+    path = "diocleziano10.jpg",
+    text = [[
+Aula VIII.
+]],
+}, {
+    path = "diocleziano11.jpg",
+}, {
+    path = "diocleziano12.jpg",
+}, {
+    path = "diocleziano13.jpg",
+    text = [[
+Aula XI — Mosaico di Ercole.
+]],
+}, {
+    path = "diocleziano14.jpg",
+    text = [[
+Art piece in gold from Dacia (modern Romania), on the eastern confines of the
+Empire.
+]],
+}, {
+    path = "diocleziano15.jpg",
+    text = [[
+ΓΝΩΘΙ ΣΑΥΤΟΝ
+]],
+}, {
+    path = "diocleziano16.jpg",
+    text = [[
+Difficult to capture in a picture the dimensions of it.  Those are bricks, so
+it's <em>big</em>.
+]],
+}, {
+    path = "diocleziano17.jpg",
+    text = [[
+Aula X — Tomba di Sulpicio Platorini.
+]],
+}, {
+    path = "diocleziano18.jpg",
+}, {
+    path = "diocleziano19.jpg",
+}, {
+    path = "diocleziano20.jpg",
+    text = [[
+Front view from Piazza della Repubblica.
+]],
+}, {
+    path = "diocleziano21.jpg",
+    text = [[
+Basilica di Santa Maria degli Angeli e dei Martiri.
+]],
+}, {
+    path = "diocleziano22.jpg",
+    text = [[
+Apse of the basilica, whose entire structure is embedded into the ancient walls
+of the baths.
+]],
+}, {
+    path = "diocleziano23.jpg",
+    text = [[
+Piazza della Repubblica and its famous Fontana delle Naiadi, with the baths
+behind it.
+]],
+}}
+
+return {
+    id = "terme-di-diocleziano",
+    title = "Terme di Diocleziano",
+    timestamp = {"1709337600", "2024-03-02"},
+    description = description,
+    content = content,
+    citation = citation,
+    images = images,
+    links = {
+        {"website", "https://museonazionaleromano.beniculturali.it"},
+        map = "way/201568922",
+        mastodon = "113669983914659352",
+        facebook = "bruno.barcarolguimaraes/posts/pfbid02hfS5YTTN8K7hsbLdcST2fk3hMEfDxi5NriBh3dG8tbgDYyk41ABvYeJLu2UGSDVzl",
+        instagram = "DDsTIopIDzZ",
+    },
+}
