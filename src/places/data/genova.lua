71d767e9ff05e48720457d9265729a0d5c0361b6
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2025-03-05 01:12:53 +0100

    places: Genova

---
+++ src/places/data/genova.lua
@@ -0,0 +1,195 @@
+local description <const> = [[Legend says the city was founded by the god Janus, whose two faces watch over past and future — the ruler of time, of gates and passages, of beginnings and endings, of traveling, commerce, and sailing, of war and peace, of morning, birth, journey, transition, and change (it was in honor of him that the Romans named the first month of the year, <i lang="la">Ianuarius</i>, which marks the winter solstice and the transition between years).  This is attested by <span lang="it">Pozzo di Giano</span>, a well in what is considered the founding place of the city of Genova which, like the two-faced Janus, faces both sea and mountain.]]
+
+local content <const> = lines {
+    par [[
+Legend says the city was founded by the god Janus, whose two faces watch over
+past and future — the ruler of time, of gates and passages, of beginnings and
+endings, of traveling, commerce, and sailing, of war and peace, of morning,
+birth, journey, transition, and change (it was in honor of him that the Romans
+named the first month of the year, <i lang="la">Ianuarius</i>, which marks the
+winter solstice and the transition between years).  This is attested by <span
+lang="it">Pozzo di Giano</span>, a well in what is considered the founding place
+of the city of Genova which, like the two-faced Janus, faces both sea and
+mountain.
+]],
+    par [[
+The pictures here are from the three times I have been there — so far — all of
+which, appropriately, marked some of the most consequential transitions in my
+life.  The most significant was exactly one year ago, as I write this, when I
+passed it moving from Lazio to a completely new life — not least because it was
+now <em>my country</em> — in my current home, Torino, about 125km north-west.
+]],
+    par [[
+The people of Liguria are of Celtic origin, distinct from the Greeks, Etruscans,
+Romans, and other historic peoples of the Italian peninsula, and the region to
+this day has a culture of its own.  Genova, as many Italian city-states, was a
+large maritime republic for many centuries, and expanded to many areas along the
+Mediterranean and beyond.  To cite a few examples:
+]],
+    ul {
+        par [[
+the Galata district of <a href="/places/istanbul.html">Istanbul</a> was a
+Genovese colony in the XIII century (when it was still Constantinople), and its
+emblematic Galata Tower was built during this period
+]],
+        par [[
+though the origins of Christopher Columbus are disputed, one of the claims is
+that he was born in Genova, the house where he lived is conserved as a museum
+and a monument can be found right in front of the train station
+]],
+    },
+    par [[
+Much of the splendor of that time is still visible in its many palaces,
+archways, and historic buildings, but it is also possible to get lost in the
+labyrinth of medieval alleyways, or in the steep hills with views of the
+Mediterranean and the beginning of the Alps, or in the amazing food of the
+original home of focaccia and pesto.
+]],
+}
+
+local citation <const> = lines {
+    text_tag("p", {lang = "la"}, [[
+Videbis ergo imperiosam urbem lapidosi collis in latere, virisque et menibus
+superbam, quam dominam maris aspectus ispse pronunciat.
+]]),
+    par [[
+You will thus see an imperious city on the side of a lapidary hill, superb by
+men and walls, whose very aspect declares herself master of the sea.
+]],
+    quote_footer("Francesco Petrarca", "Itinerarium Syriacum"),
+}
+
+local images <const> = {{
+    path = "genova0.jpg",
+    text = [[
+Part of the old town and the port, seen from my favorite place in the city:
+<a href="https://www.openstreetmap.org/way/9961020" lang="it">Villetta di
+Negro</a>.
+]],
+}, {
+    path = "genova1.jpg",
+    text = [[
+<a href="https://www.openstreetmap.org/way/154545927" lang="it">Castello
+d'Albertis</a>.
+]],
+}, {
+    path = "genova2.jpg",
+    text = [[
+View of the gulf and port of Genova from the castle.
+]],
+}, {
+    path = "genova3.jpg",
+    text = [[
+From the beautiful park of <a href="https://www.openstreetmap.org/way/25501638"
+lang="it">Villa Rosazza</a>.
+]],
+}, {
+    path = "genova4.jpg",
+    text = [[
+<a href="https://www.openstreetmap.org/way/41677804" lang="it">Sottomarino
+Nazario Sauro</a> next to the aquarium.
+]],
+}, {
+    path = "genova5.jpg",
+    text = [[
+<a href="https://www.openstreetmap.org/way/245280430" lang="it">Lanterna di
+Genova</a>.
+]],
+}, {
+    path = "genova6.jpg",
+}, {
+    path = "genova7.jpg",
+    text = [[
+The lighthouse, at 117m from sea level (the highest in the world from the time
+it was built in 1543 until 1902), watches over the entire port.
+]],
+}, {
+    path = "genova8.jpg",
+}, {
+    path = "genova9.jpg",
+    text = [[
+Ruins of <a href="https://www.openstreetmap.org/way/34886346" lang="it">Chiostro
+di Sant'Andrea</a>.
+]],
+}, {
+    path = "genova10.jpg",
+    text = [[
+Museum of <a href="https://www.openstreetmap.org/way/198253017" lang="it">Casa
+di Cristoforo Colombo</a>.
+]],
+}, {
+    path = "genova11.jpg",
+    text = [[
+<a href="https://www.openstreetmap.org/way/28846464" lang="it">Piazza Raffaele
+de Ferrari</a>.
+]],
+}, {
+    path = "genova12.jpg",
+    text = [[
+Giuseppe Garibaldi (another Ligurian).
+]],
+}, {
+    path = "genova13.jpg",
+    text = [[
+Statue of Niccolò Paganini (born in Genova) in front of <a
+href="https://www.openstreetmap.org/way/24033163" lang="it">Teatro Carlo
+Felice</a>.
+]],
+}, {
+    path = "genova14.jpg",
+    text = [[
+Villetta di Negro.
+]],
+}, {
+    path = "genova15.jpg",
+}, {
+    path = "genova16.jpg",
+    text = [[
+<span lang="it">Grotta artificiale</span> in the middle of the park.
+]],
+}, {
+    path = "genova17.jpg",
+    text = [[
+<a href="https://www.openstreetmap.org/way/276696400" lang="it">Galleria
+Giuseppe Mazzini</a>.
+]],
+}, {
+    path = "genova18.jpg",
+    text = [[
+<a href="https://www.openstreetmap.org/way/42116330" lang="it">Arco ai
+Caduti</a>.
+]],
+}, {
+    path = "genova19.jpg",
+}, {
+    path = "genova20.jpg",
+}, {
+    path = "genova21.jpg",
+    text = [[
+<a href="https://www.openstreetmap.org/way/610430306" lang="it">Basilica di
+Santa Maria Assunta di Carignano</a>.
+]],
+}, {
+    path = "genova22.jpg",
+}, {
+    path = "genova23.jpg",
+    text = [[
+The type of souvenir I am sure to buy wherever I go.
+]],
+}}
+
+return {
+    id = "genova",
+    title = "Genova",
+    timestamp = {"1714176000", "2024-04-27"},
+    description = description,
+    content = content,
+    citation = citation,
+    images = images,
+    links = {
+        map = "relation/44875",
+        mastodon = "114107008556168342",
+        facebook = "bruno.barcarolguimaraes/posts/pfbid02im9S6biF15d2gFRYZLb7twk33kjuqtPKnp1R3JAYX2mqQCTt5XmLcSyd6pejrQRtl",
+        instagram = "DGzEvaWorOC",
+    },
+}
