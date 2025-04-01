b0f80f052a969b97c44cadbbec0bbca38b32172e
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2025-04-02 00:47:22 +0200

    places: Portofino

---
+++ src/places/data/portofino.lua
@@ -0,0 +1,228 @@
+local description <const> = [[The <a href="https://bbguimaraes.com/places/genova.html">city of Ianus</a> keeps teaching me about movement and change. Ever since my first visit (of many) to Genova, I wanted to explore the coast around it, the <i lang="it">riviera ligure</i>. This weekend, after seeing that the gulf of Tigullio is only about 30km away, and that spring weather had arrived with gusto, I did what any sensible man would: got myself a bicycle and departed the very next day.]]
+
+local content <const> = lines {
+    par [[
+The <a href="genova.html">city of Ianus</a> keeps teaching me about movement and
+change.  Ever since my first visit (of many) to Genova, I wanted to explore the
+coast around it, the <i lang="it">riviera ligure</i>.  This weekend, after
+seeing that the gulf of Tigullio is only about 30km away, and that spring
+weather had arrived with gusto, I did what any sensible man would: got myself a
+bicycle and departed the very next day.
+]],
+    par [[
+The trip amounted to about 40km, over two days:
+]],
+    ul {
+        "25km: Genova – Recco",
+        "10km: Rapallo – Portofino",
+        "5km: Portofino – Santa Margherita Ligure",
+    },
+    par [[
+(I arrived in Recco with no time, and neither my legs nor the brakes of the
+bicycle were in condition, to face the trip across Monte Pollone to Rapallo as I
+had originally planed, so I ended up making those final 10km by train)
+]],
+    par [[
+Portofino is a tiny <i lang="it">comune</i>, with around 350 inhabitants.  It is
+a natural harbor, its promontory providing protection from both sea and wind and
+creating a calm pool of azure water, long used by fishermen before it became a
+big touristic attraction, and a prominent position in the gulf for both its
+lighthouse and castle.
+]],
+    par [[
+The origin of its name is found in Pliny the Elder (the one who bravely died
+during the eruption of <a href="vesuvius.html">Vesuvius</a>, again): <i
+lang="la">Portus Delphíní</i>, from the Latin word which is the root of
+“dolphin” in English.  <span lang="grc">Δελφίς</span> in turn comes from the
+word for “womb”, <span lang="grc">δελφύς</span> (since it is an aquatic mammal).
+The resemblance to the name of the famous temple — whose priestess prophesied
+the wisdom of Socrates, the fate of Oedipus, etc. — is not accidental: it was an
+ancient place of veneration of the Earth goddess Gaia, and legend says Apollo
+turned into a dolphin to carry its priests from the island of Crete, who then
+worshipped him as <span lang="grc">Απόλλων Δελφίνιος</span>.
+]],
+}
+
+local citation <const> = lines {
+    text_tag("p", {lang = "la"}, [[
+Ac ne forte roges quo me duce, quo lare tuter:<br />
+nullius addictus iurare in verba magistri,<br />
+quo me cumque rapit tempestas, deferor hospes.
+]]),
+    par [[
+And if you may ask who guides me, what place shelters me:<br />
+I do not submit to swearing by anyone's precepts,<br />
+and whither the tempest carries me off, I am borne a stranger.
+]],
+    quote_footer(
+        "Quintus Horatius Flaccus",
+        link {
+            href = "https://www.perseus.tufts.edu/hopper/text?doc=Perseus%3Atext%3A2008.01.0539%3Abook%3D1%3Apoem%3D1",
+            content = 'Epistulae',
+        },
+        "I.I"),
+}
+
+local images <const> = {{
+    path = "portofino/0.jpg",
+    text = [[
+Monte Pollone and the promontory of Portofino from Bogliasco.
+]],
+}, {
+    path = "portofino/1.jpg",
+    text = [[
+Departing from Genova.
+]],
+}, {
+    path = "portofino/2.jpg",
+    text = [[
+First bath in Punta Vagno.
+]],
+}, {
+    path = "portofino/3.jpg",
+    text = [[
+Boccadasse.
+]],
+}, {
+    path = "portofino/4.jpg",
+    text = [[
+Ponte romano di Nervi.
+]],
+}, {
+    path = "portofino/5.jpg",
+    text = [[
+Train in Bogliasco.
+]],
+}, {
+    path = "portofino/6.jpg",
+    text = [[
+Recco.
+]],
+}, {
+    path = "portofino/7.jpg",
+}, {
+    path = "portofino/8.jpg",
+}, {
+    path = "portofino/9.jpg",
+    text = [[
+Rapallo at dusk.
+]],
+}, {
+    path = "portofino/10.jpg",
+}, {
+    path = "portofino/11.jpg",
+}, {
+    path = "portofino/12.jpg",
+    text = [[
+The promontory from Rapallo.
+]],
+}, {
+    path = "portofino/13.jpg",
+    text = [[
+Chiosco della musica.
+]],
+}, {
+    path = "portofino/14.jpg",
+}, {
+    path = "portofino/15.jpg",
+}, {
+    path = "portofino/16.jpg",
+    text = [[
+The port of Rapallo, towards La Spezia.
+]],
+}, {
+    path = "portofino/17.jpg",
+    text = [[
+First stop in Santa Margherita Ligure.
+]],
+}, {
+    path = "portofino/18.jpg",
+    text = [[
+The water color announces the imminent arrival.
+]],
+}, {
+    path = "portofino/19.jpg",
+    text = [[
+Turning back towards Rapallo.
+]],
+}, {
+    path = "portofino/20.jpg",
+}, {
+    path = "portofino/21.jpg",
+}, {
+    path = "portofino/22.jpg",
+    text = [[
+La Piazzetta.
+]],
+}, {
+    path = "portofino/23.jpg",
+    text = [[
+Portofino.
+]],
+}, {
+    path = "portofino/24.jpg",
+    text = [[
+Walking the park around Castello Brown.
+]],
+}, {
+    path = "portofino/25.jpg",
+}, {
+    path = "portofino/26.jpg",
+}, {
+    path = "portofino/27.jpg",
+}, {
+    path = "portofino/28.jpg",
+}, {
+    path = "portofino/29.jpg",
+    text = [[
+The Alps, always unmistakably visible from the Ligurian coast, on the other side
+of the gulf.
+]],
+}, {
+    path = "portofino/30.jpg",
+    text = [[
+Faro di Portofino.
+]],
+}, {
+    path = "portofino/31.jpg",
+    text = [[
+How can one resist?
+]],
+}, {
+    path = "portofino/32.jpg",
+}, {
+    path = "portofino/33.jpg",
+}, {
+    path = "portofino/34.jpg",
+}, {
+    path = "portofino/35.jpg",
+}, {
+    path = "portofino/36.jpg",
+}, {
+    path = "portofino/37.jpg",
+    text = [[
+Beautiful painting by local artist <a href="https://www.maraportofino.it">Mara
+Sanguineti</a>.
+]],
+}, {
+    path = "portofino/38.jpg",
+    text = [[
+Saying goodbye from Santa Margherita Ligure, after another swim.
+]],
+}}
+
+return {
+    id = "portofino",
+    title = "Portofino",
+    timestamp = {"1743446292", "2025-03-31"},
+    description = description,
+    content = content,
+    citation = citation,
+    images = images,
+    links = {
+        map = "relation/43028",
+        mastodon = "114265184924967247",
+        facebook = "bruno.barcarolguimaraes/posts/pfbid02jzNEnSU2MvYMyyyL176xsuShUkKdaW9DJAYAdCtt3ifZr2zg2n1mE5v1M4RtYX3el",
+        instagram = "DH7BVhHI45C",
+    },
+}
