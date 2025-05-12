2f3f82e56dbd1ad27b0bfea49343509f38243e2c
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2025-05-12 20:36:37 +0200

    places: Favignana

---
+++ src/places/data/favignana.lua
@@ -0,0 +1,226 @@
+local description <const> = [[
+My first stop in Sicily — and I could not have chosen a better one — was Favignana, the largest of the <a href="https://www.openstreetmap.org/relation/1405747">Aegadian Islands</a>. These are an archipelago of five islands in the Strait of Sicily, the closest about 7km from its western end, in the region of Trapani and Marsala. The Greeks called them <span lang="grc">Aἰγάται Νῆσοι</span> (<i>Aigátai Nésoi</i>, <span lang="la">Aegates Insulae</span> in Latin), the “islands of the goats” (making them etymologicaly related to <a href="https://bbguimaraes.com/places/capri.html">Capri</a>), and according to English novelist Samuel Butler, author of a distinguished translation of Homer's Odyssey in 1900, their Ἱερά Νήσος (<i>Hierá Nésos</i>, “holy island”), the island of Marettimo, furthest from the coast, is none other than Ithaca, the famous homeland that Odysseus seeks throughout the epic.
+]]
+
+local content <const> = lines {
+    par [[
+My first stop in Sicily — and I could not have chosen a better one — was
+Favignana, the largest of the <a
+href="https://www.openstreetmap.org/relation/1405747">Aegadian Islands</a>.
+These are an archipelago of five islands in the Strait of Sicily, the closest
+about 7km from its western end, in the region of Trapani and Marsala.  The
+Greeks called them <span lang="grc">Aἰγάται Νῆσοι</span> (<i>Aigátai Nésoi</i>,
+<span lang="la">Aegates Insulae</span> in Latin), the “islands of the goats”
+(making them etymologicaly related to <a href="capri.html">Capri</a>), and
+according to English novelist Samuel Butler, author of a distinguished
+translation of Homer's Odyssey in 1900, their Ἱερά Νήσος (<i>Hierá Nésos</i>,
+“holy island”), the island of Marettimo, furthest from the coast, is none other
+than Ithaca, the famous homeland that Odysseus seeks throughout the epic.
+]],
+    par [[
+Regardless of the veracity of this claim, the islands have significant
+historical importance, as can be seen from the archaeological remains from the
+Paleolithic Era (back in the glacial period when all of Italy, Sicily, and its
+islands were connected and more easily reachable from Africa) and the Punic wars
+found all over the surrounding sea.  Even though I would have liked to visit
+them all, the short time available to stay in each place meant I had to make
+sacrifices: here I had to choose only one of the islands.
+]],
+    par [[
+Favignana ended up being my choice; not that it is any less illustrious than its
+sisters: it was the famous site of the major naval victory of the Romans over
+the Carthaginians in 241 B.C., which marked the end of the first Punic war and
+the annexation of Sicily as their first province, later establishing Rome's
+dominance as the supreme ruler of the Mediterranean, surpassing even the mighty
+Greeks, who had had their own conflicts with the same enemies for centuries at
+this point.  All this by the somewhat obscure general Gaius Lutatius Catulus, a
+<i lang="la">novus homó</i> who won this battle only to fade again into history.
+This event is still celebrated in Favignana more than two thousand years later —
+and I have <em>two</em> shirts to prove it (this is why I love Italy).
+]],
+    html "<hr />",
+    par [[
+Favignana is the perfect exemplar of the Scilian islands.  Other than monte
+Santa Caterina at its center, the land is mostly flat, making its coast ideal to
+be explored by walking or cycling.  And cycle I did: in one afternoon, I went
+through its entire 33km perimeter.  The reward for doing so are some of the most
+surreal landscapes in all of Italy.
+]],
+    par [[
+The islands of Sicily are miniatures of itself, with mountains descending
+steeply into beaches of the crystal-clear water of the central Mediterranean,
+with volcanic rocks and exotic, exuberant vegetation all over them.  I have
+never seen sea water so transparent, and of an azure-turquoise color so intense
+it is almost offensive to the senses.
+]],
+    par [[
+A curious feature of the island are the ever-present quarries: colossal sections
+of rock (calcarenite, a type of limestone) which have been cut in precise right
+angles to be used for buildings — the famous Duomo di Monreale is one example of
+a construction done using limestone from Favignana.  It was historically an
+important source of this type of material, and these sites can still be seen
+throughout the island, including and especially along the coast, where caves
+were opened in cliffs right next to the sea.
+]],
+    par [[
+In some places the protection from wind and abundance of sun and water provided
+by these artificially lowered surfaces have created the conditions for rich
+vegetation to develop — the famous <i>hypogeum gardens</i> of Favignana (from
+Greek <span lang="grc">ὑπο- + γῆ + -ῐος</span>, “under the ground”).
+]],
+}
+
+local citation <const> = lines {
+    text_tag("p", {lang = "grc"}, [[
+εἴµ' Ὀδυσεὺς Λαερτιάδης, ὃς πᾶσι δόλοισιν<br />
+ἀνθρώποισι µέλω, καί µευ κλέος οὐρανὸν ἵκει.<br />
+ναιετάω δ' Ἰθάκην ἐυδείελον: ἐν δ' ὄρος αὐτῇ<br />
+Νήριτον εἰνοσίφυλλον, ἀριπρεπές: ἀµφὶ δὲ νῆσοι<br />
+πολλαὶ ναιετάουσι µάλα σχεδὸν ἀλλήλῃσι,<br />
+∆ουλίχιόν τε Σάµη τε καὶ ὑλήεσσα Ζάκυνθος.<br />
+αὐτὴ δὲ χθαµαλὴ πανυπερτάτη εἰν ἁλὶ κεῖται<br />
+πρὸς ζόφον, αἱ δέ τ' ἄνευθε πρὸς ἠῶ τ' ἠέλιόν τε,
+]]),
+    par [[
+I am Odysseus, Laertes’ son, world-famed<br />
+For stratagems: my name has reached the heavens.<br />
+Bright Ithaca is my home: it has a mountain,<br />
+Leaf-quivering Neriton, far visible.<br />
+Around are many islands, close to each other,<br />
+Doulichion and Same and wooded Zacynthos.<br />
+Ithaca itself lies low, furthest to sea<br />
+Towards dusk; the rest, apart, face dawn and sun.
+]],
+    quote_footer(
+        "Homer",
+        "Odyssey",
+        link {
+            href = "https://www.perseus.tufts.edu/hopper/text?doc=Hom.+Od.+9.19&amp;fromdoc=Perseus%3Atext%3A1999.01.0136",
+            content = "9.19-26",
+        }),
+}
+
+local images <const> = {{
+    path = "favignana/0.jpg",
+    text = [[
+The surreal waters of Cala Rossa.
+]],
+}, {
+    path = "favignana/1.jpg",
+    text = [[
+Arrival of the traghetto.
+]],
+}, {
+    path = "favignana/2.jpg",
+    text = [[
+The port, with the castle of monte Santa Catarina behind it.
+]],
+}, {
+    path = "favignana/3.jpg",
+    text = [[
+The neighboring island of Levanzo, from via Frascia.
+]],
+}, {
+    path = "favignana/4.jpg",
+}, {
+    path = "favignana/5.jpg",
+    text = [[
+Northern end of Monte Santa Catarina.
+]],
+}, {
+    path = "favignana/6.jpg",
+    text = [[
+Nespole siciliane.
+]],
+}, {
+    path = "favignana/7.jpg",
+    text = [[
+The quarry of Cala Graziosa.
+]],
+}, {
+    path = "favignana/8.jpg",
+}, {
+    path = "favignana/9.jpg",
+    text = [[
+Trapani and Monte Erice, in the main island of Sicily, visible in the far
+distance.
+]],
+}, {
+    path = "favignana/10.jpg",
+}, {
+    path = "favignana/11.jpg",
+    text = [[
+Cala Rossa.
+]],
+}, {
+    path = "favignana/12.jpg",
+}, {
+    path = "favignana/13.jpg",
+}, {
+    path = "favignana/14.jpg",
+}, {
+    path = "favignana/15.jpg",
+    text = [[
+Spiaggia Bue Marino.
+]],
+}, {
+    path = "favignana/16.jpg",
+}, {
+    path = "favignana/17.jpg",
+}, {
+    path = "favignana/18.jpg",
+    text = [[
+Inside one of the huge artificial caves next to that beach.
+]],
+}, {
+    path = "favignana/19.jpg",
+}, {
+    path = "favignana/20.jpg",
+    text = [[
+The old strada provinciale which circles monte Santa Catarina.
+]],
+}, {
+    path = "favignana/21.jpg",
+}, {
+    path = "favignana/22.jpg",
+    text = [[
+Punta Faraglione.
+]],
+}, {
+    path = "favignana/23.jpg",
+}, {
+    path = "favignana/24.jpg",
+    text = [[
+Back at the port at dusk.
+]],
+}, {
+    path = "favignana/25.jpg",
+}, {
+    path = "favignana/26.jpg",
+    text = [[
+My companion, which took me all along the island that day.
+]],
+}, {
+    path = "favignana/27.jpg",
+    text = [[
+Recovering with a delicious risotto alla marinara back in Trapani.
+]],
+}, {
+    path = "favignana/28.jpg",
+}}
+
+return {
+    id = "favignana",
+    title = "Favignana",
+    timestamp = {"1745280000", "2025-04-22"},
+    description = description,
+    content = content,
+    citation = citation,
+    images = images,
+    links = {
+        map = "relation/1405747",
+        mastodon = "114496481942055849",
+        facebook = "bruno.barcarolguimaraes/posts/pfbid0on83Pgw341BPzoCpADCFv25wEFcsGzBmCK8zbpGNBYFHHYKFZbyHWaGQDkCPNft2l",
+        instagram = "DJkKMdQuWPh",
+    },
+}
