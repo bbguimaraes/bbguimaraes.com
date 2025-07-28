76fa3771b84c0662d7a627a0b711590155b06a5f
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2025-07-28 16:08:34 +0200

    places: Perugia

---
+++ src/places/data/perugia.lua
@@ -0,0 +1,603 @@
+local description <const> = [[Recently, I was recommended a trip to Perugia to attend the <a href="https://www.umbriajazz.it">Umbria Jazz festival</a> (my friends are <i lang="it">pochi ma buoni</i>) — or rather was compelled to go the moment I saw how incredible the program was, with artists such as Herbie Hancock, and Marcus Miller, Jacob Collier all in the same festival.]]
+
+local content <const> = lines {
+    par [[
+Recently, I was recommended a trip to Perugia to attend the <a
+href="https://www.umbriajazz.it">Umbria Jazz festival</a> (my friends are <i
+lang="it">pochi ma buoni</i>) — or rather was compelled to go the moment I saw
+how incredible the program was, with artists such as Herbie Hancock, Marcus
+Miller, and Jacob Collier all in the same festival.
+]],
+    par [[
+While it was challenging to balance working, exploring the city, and watching
+all the amazing concerts (and trying to get some sleep), I still managed to run
+around and see most of the places I had planned for my — tragically short —
+four-day stay.  The result is this post, which is by far the longest I have
+written (and that is without even going into the festival itself, which will
+have to wait for another time), but that is just how amazing this place is.
+]],
+    h2_link { "people", "people" },
+    par [[
+Perugia, as many major Italian cities, is <em>ancient</em>.  Its origins go back
+at least three millennia, a fusion of Umbrian, Etruscan, and Roman history.  The
+modern center is located where the ancient Etruscan acropolis rose, about 300m
+from the valley of the <span lang="it">Tevere</span>, on top of two hills: <span
+lang=it">Colle Landone</span> and <span lang="it">Colle del Sole</span>, and is
+still delimited by two concentric defensive walls: one built by the Etruscans in
+the IV century B.C. and a second built after the expansion of the city in the
+mediaeval period.
+]],
+    par [[
+Umbria is the region right at the center of continental Italy, and its capital
+Perugia is located roughly at equal distance from <span
+lang="it">Firenze</span> and <span lang="it">Roma</span> (150km) and <span
+lang="it">Milano</span> and <span lang="it">Napoli</span> (400km).  In
+mediaeval times it was part of the “Byzantine corridor” which connected the
+Eastern Roman empire, centered in Constantinople (modern <a
+href="istanbul.html">Istanbul</a>) after the fall of Rome, to the regions it
+still held in central and southern Italy.
+]],
+    par [[
+This central position made it a place of confluence of three important cultures
+of the ancient world:
+]],
+    ul {
+        par [[
+the Umbrians, the original people of the region, who settled in the surrounding
+lands in the XI–X centuries B.C. and on the hill where Perugia was built in the
+VIII century,
+]],
+         par [[
+the Etruscans (the inhabitants of Etruria, modern <span
+lang="it">Toscana</span>), who refounded the city with their Umbrian neighbors
+around the VI century, making it one of the largest of their territory, and
+eventually formed an (ultimately unsuccessful) alliance against…
+]],
+        par [[
+the Romans, who incorporated both Etruscans and Umbrians in the III century into
+their ever-increasing empire, not only geographically and politically, but also
+adopting many of their cultural aspects — the Etruscan contribution to Roman
+culture is comparable to that of the Greeks.
+]],
+    },
+    par [[
+In fact, the river Tiber, which flows from the southern end of Emilia-Romagna
+all the way to the port of Ostia in Rome, was for centuries the dividing line
+between the ancient regions of Etruria and Umbria, and Perugia sits right
+between them.  Those who have been to Tuscany will certainly recognize a similar
+style.
+]],
+    par [[
+The perfect place to learn about this is <span lang="it">Museo Archeologico
+Nazionale dell'Umbria</span>, in <span lang="it">rione San Pietro</span> on the
+south-east side of the center, right next to <span lang="it">Basilica di San
+Domenico</span>, which houses, among many other historical artifacts, one of
+the longest intact Etruscan inscriptions ever found, the III century <i
+lang="la">Cippus Peruginus</i>.
+]],
+    h2_link { "walls", "walls" },
+    par [[
+A striking features of the city are the walls that surround the historical
+center, with beautifully preserved monumental gates which provide passage in
+and out of it.  The inner wall, with a circumference of about 3km, is less
+visible as parts of it were incorporated into other buildings over time.
+However, it contains the most distinctive of all the gates: <span
+lang="it">Arco Etrusco</span>.
+]],
+    par [[
+Opening at the northern end of the old acropolis into the <span lang="la">cardo
+maximus</span> (the main north–south road of Roman cities, where today are
+located via Ulisse Rocchi, where I stayed, and corso Pietro Vannucci, the main
+street of the city center), its construction is in the same megalithic style
+seen in most Etruscan works, with large marble stones fit together by precise
+cutting, without the use of any binding.  The arch bears the inscription
+“Augusta Perusia”, the name given by the emperor Augustus after his victory in
+the Perusine War (won only after a seven-month siege, a testament to the
+impenetrability of the city's defenses).
+]],
+    par [[
+The outer wall, about 9km in length, is a clearer delimitation of the city
+center, and is where most of the gates can still be seen.  Five of them give
+their name to the five <i lang="it">rioni</i>, the regions into which the old
+center is divided: <span lang="it">Porta Sant'Angello, Porta Sole, Porta San
+Pietro, Porta Eburnea, and Porta Santa Susanna</span>.
+]],
+    h2_link { "art", "art" },
+    par [[
+Beyond the classical period, Perugia was also a cultural center of the
+Renaissance, and many of the greatest works of the period can still be seen
+around the city, specially at the <span lang="it">Galleria Nazionale
+dell'Umbria</span> (which sadly I did not have time to visit).
+]],
+    par [[
+Pietro Vannucci, one of the masters of the early Italian Renaissance movement,
+was born in what today is <span lang="it">provincia di Perugia</span> and spent
+much of his life in the city, resulting in his eponym,
+<i lang="it">Perugino</i>.
+]],
+    par [[
+One of his pupils was <span lang="it">Raffaello</span>, regarded as one of the
+three greatest figures (along with Michelangelo and Da Vinci) of the High
+Renaissance period.  Born in the <span lang="it">Marche</span>, he moved to
+Perugia in 1494, at 11 years old, to develop his craft under Perugino, and one
+of their collaborative works can still be seen here: the fresco <i
+lang="it">Trinità e santi</i> in <span lang="it">Cappella San Severo</span>,
+which displays the unmistakable style that can be seen in his later works, such
+as the <i>Scuola di Atene</i> in the Vatican.
+]],
+    h2_link { "around-the-city", "around the city" },
+    par [[
+Just walking around the historical center, admiring the architecture in Etruscan
+style, the wide views over the landscape of the valley, or simply the
+picturesque scenery of an old Italian town, is an attraction in itself.  At
+every corner along the way there is something beautiful to stop and admire.
+]],
+    par [[
+Venturing just north of the main square one finds a curious path which passes
+under via Cesare Battisti, down a large stairway, then over several streets on
+its way north-west.  This is in fact an ancient aqueduct, an engineering marvel
+from the XIII century, which brought water from monte Pacciano, about 3–4km
+north, to the top of the Perugian hill, providing water for the city, including
+the famous <span lang="it">Fontana Maggiore</span>, through the use of pressure
+alone, without the aid of pumps.  It later fell in disuse, but part of its
+course has been turned into <span lang="it">via dell'Acquedotto</span>, a scenic
+course connecting <span lang="it">rione Porta Sant'Angelo</span> to the city
+center.
+]],
+    par [[
+Not far from the fountain is a much older but equally amazing exemplar of
+aquatic engineering: <span lang="it">Pozzo Etrusco</span>.  This is a well built
+in the III century B.C. by the Etruscans at the top of <span lang="it">Colle del
+Sole</span>.  The underground of palazzo Sorbello has since been turned into a
+small museum, which can be visited along with the well itself.  It is an amazing
+display of the engineering abilities of the Etruscans, penetrating 40m into the
+hill, with a capacity to hold almost 500,000 liters of water.  The top
+structure, just below the street, is supported by two sets, each weighing nearly
+a tonne, of four limestone blocks, also here set in place without the use of
+mortar or other type of binding (there is a great depiction in the museum of how
+this engineering feat was accomplished).
+]],
+    h2_link { "churches", "churches" },
+    par [[
+In the northern region, I visited what has become my favorite among all the
+churches I have been to (and those are a quite few): chiesa di San Michele
+Arcangelo.  Because it is in fact a paleochristian temple from the V century
+(likely erected over even older Roman and Etruscan temples), it has an unusual
+style: its shape is round, and the presbytery is divided from the ambulatory by
+a circle of sixteen Roman marble columns in Corinthian style.  Light enters the
+building primarily from twelve windows placed around the cupola, which sits just
+above the altar, rendering the interior even more austere.
+]],
+    par [[
+If that were not enough, not only is the traditional mass in Latin still
+celebrated there, the temple sits on the so-called <i>Line of Saint Michael</i>,
+a series of temples dedicated to the archangel which form a straight line from
+Ireland to Israel, aligned with the last ray of sunlight of the summer solstice.
+]],
+    par [[
+The cathedral, built in the XV century, stands right on the main square, next to
+<span lang="it">Palazzo dei Priori</span> and <span lang="it">Fontana
+Maggiore</span>, and contains many works of the Renaissance, as well as what
+tradition holds to be the <i>Holy Ring</i>, the wedding ring of the Virgin Mary
+and Saint Joseph.
+]],
+    par [[
+A short distance south past the city walls is <span lang="it">Basilica di San
+Pietro</span>, which gives its name to both the region where it stands and the
+gate which leads to it.  A stunning X century building, its 70m-high bell tower
+is the tallest construction in Perugia and one of its most distinctive.  A
+favorite of mine, it also contains both a botanical garden and a reproduction of
+a mediaeval <i lang="la">hortus conclusus</i>, a symbolic monastic garden.
+]],
+    par [[
+On the western end of the center is <span lang="it">Oratorio di San
+Bernardino</span> and <span lang="it">Chiesa di San Francesco al Prato</span>,
+the latter a XIII century church built in similar style to the church of Santa
+Chiara in the city of Assisi which, just 25km east, can be seen from many points
+in <span lang="it">Colle del Sole</span>.
+]],
+}
+
+local citation <const> = lines {
+    text_tag("p", {lang = "grc"}, [[
+ὅτι καὶ χαλεπώτερον δουλεύειν πάθεσιν ἢ τυράννοις· ἀδύνατον δ' εἶναι ἐλεύθερον
+τὸν ὑπὸ παθῶν κρατούμενον.
+]]),
+    par [[
+For the rule of the passions is harder than that of tyrants, since it is
+impossible for a man to be free who is governed by his passions.
+]],
+    quote_footer("Porphyry", "Letter to Marcella", "34"),
+}
+
+local images <const> = {{
+    path = "perugia/0.jpg",
+    text = [[
+<span lang="it">Arco Etrusco</span>.
+]],
+}, {
+    path = "perugia/1.jpg",
+    text = [[
+<span lang="it">Rione San Pietro</span> from <span lang="it">Giardini
+Carducci</span>.
+]],
+}, {
+    path = "perugia/2.jpg",
+    text = [[
+A different face each day.
+]],
+}, {
+    path = "perugia/3.jpg",
+}, {
+    path = "perugia/4.jpg",
+    text = [[
+The eastern side of the valley covered in fog.
+]],
+}, {
+    path = "perugia/5.jpg",
+    text = [[
+Assisi in the distance at the foot of Monte Subasio.
+]],
+}, {
+    path = "perugia/6.jpg",
+    text = [[
+View of the southern plain, with the arena and <span lang="it">Chiesa di Santa
+Giuliana</span> in sight.
+]],
+}, {
+    path = "perugia/7.jpg",
+    text = [[
+Via Luigi Massi.
+]],
+}, {
+    path = "perugia/8.jpg",
+    text = [[
+<span lang="it">Colle del Sole</span> from <span lang="it">Università degli
+Studi di Perugia</span>.
+]],
+}, {
+    path = "perugia/9.jpg",
+    text = [[
+<span lang="it">Giardini Carducci</span> ready to host the festival later that
+day.
+]],
+}, {
+    path = "perugia/10.jpg",
+}, {
+    path = "perugia/11.jpg",
+    text = [[
+<span lang="it">Tagliata di bavetta</span>.  I have to mention this was at <a
+lang="it" href="https://www.locandadelbartoccio.it">Locanda del Bartoccio</a>,
+since they informed me this plate <span lang="it">“si può fare solo al
+sangue”</span>.  That's when I knew I had come to the right place.
+]],
+}, {
+    path = "perugia/12.jpg",
+    text = [[
+<span lang="it">Fontana Maestà delle Volte</span>.
+]],
+}, {
+    path = "perugia/13.jpg",
+}, {
+    path = "perugia/14.jpg",
+    text = [[
+<span lang="it">Arco di Via delle Volte</span>.
+]],
+}, {
+    path = "perugia/15.jpg",
+}, {
+    path = "perugia/16.jpg",
+    text = [[
+<span lang="it">Porta San Pietro</span>.
+]],
+}, {
+    path = "perugia/17.jpg",
+}, {
+    path = "perugia/18.jpg",
+}, {
+    path = "perugia/19.jpg",
+}, {
+    path = "perugia/20.jpg",
+    text = [[
+<span lang="it">Basilica di San Pietro</span>.
+]],
+}, {
+    path = "perugia/21.jpg",
+}, {
+    path = "perugia/22.jpg",
+}, {
+    path = "perugia/23.jpg",
+}, {
+    path = "perugia/24.jpg",
+}, {
+    path = "perugia/25.jpg",
+}, {
+    path = "perugia/26.jpg",
+}, {
+    path = "perugia/27.jpg",
+    text = [[
+Mediaeval garden.
+]],
+}, {
+    path = "perugia/28.jpg",
+}, {
+    path = "perugia/29.jpg",
+    text = [[
+<span lang="it">Corso Pietro Vannucci</span>.
+]],
+}, {
+    path = "perugia/30.jpg",
+    text = [[
+<span lang="it">Cattedrale di San Lorenzo</span> and <span lang="it">Fontana
+Maggiore</span>.
+]],
+}, {
+    path = "perugia/31.jpg",
+}, {
+    path = "perugia/32.jpg",
+}, {
+    path = "perugia/33.jpg",
+}, {
+    path = "perugia/34.jpg",
+    text = [[
+Chapel of the Holy Ring.
+]],
+}, {
+    path = "perugia/35.jpg",
+}, {
+    path = "perugia/36.jpg",
+    text = [[
+<span lang="it">Pozzo Etrusco</span>.
+]],
+}, {
+    path = "perugia/37.jpg",
+}, {
+    path = "perugia/38.jpg",
+}, {
+    path = "perugia/39.jpg",
+}, {
+    path = "perugia/40.jpg",
+    text = [[
+<span lang="it">Rione Porta Sole</span>.
+]],
+}, {
+    path = "perugia/41.jpg",
+}, {
+    path = "perugia/42.jpg",
+    text = [[
+View from <span lang="it">Porta Sole</span>.
+]],
+}, {
+    path = "perugia/43.jpg",
+    text = [[
+<span lang="it">Rione Sant'Angelo</span>.
+]],
+}, {
+    path = "perugia/44.jpg",
+    text = [[
+<span lang="it">Cappella San Severo</span>.
+]],
+}, {
+    path = "perugia/45.jpg",
+    text = [[
+<i lang="la">Quid Raphael puber longoevus quidque valeret Petrus opus muro
+ductum testatur eodem</i>.
+]],
+}, {
+    path = "perugia/46.jpg",
+    text = [[
+<i lang="it">Trinità e santi</i>.
+]],
+}, {
+    path = "perugia/47.jpg",
+    text = [[
+Bust of <span lang="it">Raffaello</span>.
+]],
+}, {
+    path = "perugia/48.jpg",
+    text = [[
+<i lang="la">Rafael de Urbino Domino Octaviano Stephani Volate[r]ano priore
+Sanctam Trinitatem Angelos Astantes Sanctos[q.] pinxit A.D. [M]DV</i>.
+]],
+}, {
+    path = "perugia/49.jpg",
+    text = [[
+<span lang="it">Rione Sant'Angelo</span>.
+]],
+}, {
+    path = "perugia/50.jpg",
+}, {
+    path = "perugia/51.jpg",
+}, {
+    path = "perugia/52.jpg",
+    text = [[
+<span lang="it">Chiesa di San Michele Arcangelo</span>.
+]],
+}, {
+    path = "perugia/53.jpg",
+}, {
+    path = "perugia/54.jpg",
+}, {
+    path = "perugia/55.jpg",
+}, {
+    path = "perugia/56.jpg",
+}, {
+    path = "perugia/57.jpg",
+}, {
+    path = "perugia/58.jpg",
+}, {
+    path = "perugia/59.jpg",
+}, {
+    path = "perugia/60.jpg",
+    text = [[
+<span lang="it">Porta Sant'Angelo</span>.
+]],
+}, {
+    path = "perugia/61.jpg",
+}, {
+    path = "perugia/62.jpg",
+}, {
+    path = "perugia/63.jpg",
+    text = [[
+<span lang="it">Via dell'Acquedotto</span>.
+]],
+}, {
+    path = "perugia/64.jpg",
+}, {
+    path = "perugia/65.jpg",
+}, {
+    path = "perugia/66.jpg",
+}, {
+    path = "perugia/67.jpg",
+}, {
+    path = "perugia/68.jpg",
+}, {
+    path = "perugia/69.jpg",
+    text = [[
+<span lang="it">Piazza d'Italia</span>.
+]],
+}, {
+    path = "perugia/70.jpg",
+}, {
+    path = "perugia/71.jpg",
+    text = [[
+<span lang="it">Porta Trasimena</span>.
+]],
+}, {
+    path = "perugia/72.jpg",
+    text = [[
+<span lang="it">Oratorio di San Bernardino</span>.
+]],
+}, {
+    path = "perugia/73.jpg",
+    text = [[
+<span lang="it">Chiesa di San Francesco al Prato</span>.
+]],
+}, {
+    path = "perugia/74.jpg",
+}, {
+    path = "perugia/75.jpg",
+}, {
+    path = "perugia/76.jpg",
+}, {
+    path = "perugia/77.jpg",
+    text = [[
+<span lang="it">Arco Etrusco</span>.
+]],
+}, {
+    path = "perugia/78.jpg",
+}, {
+    path = "perugia/79.jpg",
+}, {
+    path = "perugia/80.jpg",
+    text = [[
+<span lang="it">Fonte Lomellina</span>.
+]],
+}, {
+    path = "perugia/81.jpg",
+    text = [[
+<span lang="it">Porta Marzia</span>.
+]],
+}, {
+    path = "perugia/82.jpg",
+    text = [[
+<span lang="it">Rocca Paolina</span>.
+]],
+}, {
+    path = "perugia/83.jpg",
+}, {
+    path = "perugia/84.jpg",
+}, {
+    path = "perugia/85.jpg",
+}, {
+    path = "perugia/86.jpg",
+    text = [[
+<span lang="it">Chiesa di San Domenico</span>.
+]],
+}, {
+    path = "perugia/87.jpg",
+}, {
+    path = "perugia/88.jpg",
+}, {
+    path = "perugia/89.jpg",
+    text = [[
+<span lang="it">Museo Archeologico Nazionale dell'Umbria</span>.
+]],
+}, {
+    path = "perugia/90.jpg",
+}, {
+    path = "perugia/91.jpg",
+}, {
+    path = "perugia/92.jpg",
+}, {
+    path = "perugia/93.jpg",
+}, {
+    path = "perugia/94.jpg",
+    text = [[
+<span lang="it">Cippus Peruginus</span>.
+]],
+}, {
+    path = "perugia/95.jpg",
+}, {
+    path = "perugia/96.jpg",
+    text = [[
+<span lang="it">Chiesa di Sant'Ercolano</span>.
+]],
+}, {
+    path = "perugia/97.jpg",
+}, {
+    path = "perugia/98.jpg",
+}, {
+    path = "perugia/99.jpg",
+    text = [[
+<span lang="it">Antica porchetteria Granieri</span>.
+]],
+}, {
+    path = "perugia/100.jpg",
+}, {
+    path = "perugia/101.jpg",
+    text = [[
+<span lang="it">Via Ulisse Rocchi</span>.
+]],
+}, {
+    path = "perugia/102.jpg",
+    text = [[
+<span lang="it">Rione Porta Santa Susanna</span>.
+]],
+}, {
+    path = "perugia/103.jpg",
+}, {
+    path = "perugia/104.jpg",
+}, {
+    path = "perugia/105.jpg",
+}, {
+    path = "perugia/106.jpg",
+}, {
+    path = "perugia/107.jpg",
+    text = [[
+<span lang="it">Porta San Giacomo</span>.
+]],
+}, {
+    path = "perugia/108.jpg",
+    text = [[
+<span lang="it">Stazione di Perugia Fontivegge</span>.
+]],
+}, {
+    path = "perugia/109.jpg",
+}}
+
+return {
+    id = "perugia",
+    title = "Perugia",
+    timestamp = {"1752451200", "2025-07-14"},
+    description = description,
+    content = content,
+    citation = citation,
+    images = images,
+    links = {
+        map = "relation/42278",
+        mastodon = "114931386175568055",
+        facebook = "bruno.barcarolguimaraes/posts/pfbid047umFBVvk32v7d1H6aoUBCNHQAH9fN6xnXrsveaK3DkQxRbrnyKAogSpUHotVYgDl",
+        instagram = "DMp8PSztRDs",
+    },
+}
