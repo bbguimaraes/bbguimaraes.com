52538e436bcdb1b1ab0e501cd08867f46cc3273a
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2025-12-07 18:04:50 +0100

    music: Caruso

---
+++ src/music/data/caruso.lua
@@ -0,0 +1,20 @@
+local content <const> = blockquote(text_tag("p", {lang = "it"}, [[
+Sentì il dolore nella musica, si alzò dal pianoforte<br />
+ma quando vide la luna uscire da una nuvola<br />
+gli sembrò più dolce anche la morte
+]]))
+
+return {
+    id = "caruso",
+    title = "Caruso",
+    author = "Lucio Dalla",
+    timestamp = { 1763565776, "2025-11-19T15:22:56" },
+    duration = "1:34",
+    poster = "0:47",
+    tags = {"piano", "short"},
+    links = {
+        facebook = "bruno.barcarolguimaraes/posts/pfbid0Azvertb7eWZBwVxDXrbybUEFozw9knkbwyN2yZMtbwi4AJa3sGzSHW2AS85KfhKQl",
+        instagram = "DRPbHXdDIEL/",
+    },
+    content = content,
+}
