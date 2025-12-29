b67059b1fc2937685566d6b13222e80e46cb0b9f
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2025-12-29 11:09:20 +0100

    rss: extract common function

--- src/rss.lua
+++ src/rss.lua
@@ -4,92 +4,81 @@ local path <const> = require "lib.path"
 local files <const> = {}
 local file_names <const> = {}
 
+local function generate_common(dir, file_name, t, tag)
+    local t <const> = generate.load(path.join("src", dir, "data", file_name))
+    if t.rss ~= nil and not t.rss then
+        return
+    end
+    t.file = path.join(
+        dir, file_name:gsub("%.lua$", ".html"):gsub("_", "-"), nil)
+    if t.tags then
+        table.insert(t.tags, 1, tag or dir)
+    else
+        t.tags = {tag or dir}
+    end
+    files[file_name] = t
+    table.insert(file_names, file_name)
+    return t
+end
+
 local blog_dir <const> = path.join("src", "blog", "data")
 for x in path.each(blog_dir) do
-    local t <const> = generate.load(path.join(blog_dir, x))
-    t.file = "blog/" .. x
-    table.insert(t.tags, 1, "blog")
-    t.timestamp = math.tointeger(t.timestamp[1])
-    files[x] = t
-    table.insert(file_names, x)
+    generate_common("blog", x, t)
 end
 
 local lib_dir <const> = path.join("src", "lib", "data")
 for x in path.each(lib_dir) do
-    local t <const> = generate.load(path.join(lib_dir, x))
-    local rss <const> = t.rss
-    if rss ~= nil and not rss then
+    local t <const> = generate_common("lib", x, t, "books")
+    if not t then
         goto continue
     end
     local title, author = t.title, t.author
-    local desc <const> = t.description
     if t.languages then
         title = title.en
         author = author.en
     end
-    t.file = path.join("lib", x)
-    t.tags = {"books"}
     t.title = title
     t.short_title = nil
-    if desc then
+    if t.description then
         t.description = string.format(
             "<p>%s, %s</p><p>%s</p>",
-            title, author, desc:gsub("\n$", ""):gsub("\n", " "))
+            title, author, t.description:gsub("\n$", ""):gsub("\n", " "))
     else
         t.description = string.format("%s, %s", title, author)
     end
-    t.timestamp = math.tointeger(t.timestamp[1])
-    files[x] = t
-    table.insert(file_names, x)
     ::continue::
 end
 
 local music_dir <const> = path.join("src", "music", "data")
 for x in path.each(music_dir) do
-    local t <const> = generate.load(path.join(music_dir, x))
-    local author <const>, tags <const>, description <const> =
-        t.author, t.tags, t.description
+    local t <const> = generate_common("music", x, t)
     local desc = {}
-    table.insert(desc, "<p>music")
-    for _, x in ipairs(tags) do
-        table.insert(desc, " ")
-        table.insert(desc, x)
-    end
+    table.insert(desc, "<p>")
+    table.insert(desc, table.concat(t.tags, " "))
     table.insert(desc, "</p>")
-    if author then
+    if t.author then
         table.insert(desc, "<p>")
-        table.insert(desc, author)
+        table.insert(desc, t.author)
         table.insert(desc, "</p>")
     end
-    if description then
+    if t.description then
         table.insert(desc, "<p>")
-        table.insert(desc, (description:gsub("\n$", ""):gsub("\n", " ")))
+        table.insert(desc, (t.description:gsub("\n$", ""):gsub("\n", " ")))
         table.insert(desc, "</p>")
     end
-    t.file = path.join("music", x:gsub("%.lua$", ".html"):gsub("_", "-"), nil)
-    table.insert(tags, 1, "music")
     t.description = table.concat(desc)
-    t.timestamp = math.tointeger(t.timestamp[1])
-    files[x] = t
-    table.insert(file_names, x)
 end
 
 local places_dir <const> = path.join("src", "places", "data")
 for x in path.each(places_dir) do
-    local t <const> = generate.load(path.join(places_dir, x))
-    local desc <const> = t.description
-    if desc then
-        t.description = desc:gsub("\n$", ""):gsub("\n", " ")
+    local t <const> = generate_common("places", x, t, "place")
+    if t.description then
+        t.description = t.description:gsub("\n$", ""):gsub("\n", " ")
     end
-    t.file = path.join("places", x:gsub("%.lua$", ".html"):gsub("_", "-"), nil)
-    t.timestamp = math.tointeger(t.timestamp[1])
-    t.tags = {"place"}
-    files[x] = t
-    table.insert(file_names, x)
 end
 
 table.sort(file_names, function(x, y)
-    return files[y].timestamp < files[x].timestamp
+    return files[y].timestamp[1] < files[x].timestamp[1]
 end)
 local posts <const> = {}
 for _, x in ipairs(file_names) do
