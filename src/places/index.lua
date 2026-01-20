72e5b8c494b198236fb3706aec73894267b456f9
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2026-01-20 15:52:10 +0100

    css: adjust margins

--- src/places/index.lua
+++ src/places/index.lua
@@ -83,7 +83,7 @@ end)
 return include "master.lua" {
     title = "places",
     css = {"/main.css", "/gallery.css", "places.css"},
-    body_class = "no-margin white-bg roman",
+    body_class = "no-hor-margin white-bg roman",
     main = lines {
         div({class = "w80"}, include "nav.lua" {
             nav_path = {{nil, "places"}},
