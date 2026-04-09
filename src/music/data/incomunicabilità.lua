c278d0a08276204c20394c7447d2a29ae0e12f3a
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2026-04-09 22:42:17 +0200

    music: incomunicabilità

---
+++ src/music/data/incomunicabilità.lua
@@ -0,0 +1,66 @@
+local description <const> = [[It's been a while since I last published some music.  But today I have something very unusual, in many ways: a composition of my own.]]
+
+local content <const> = lines {
+    par [[
+It's been a while since I last published some music.  I'm still playing all the
+time, especially in the past couple of months, but I'm doing so many things,
+musically and otherwise, that there had not been anything I managed to study and
+play well enough to merit a post.
+]],
+    par [[
+But today I have something very unusual, in many ways: a composition of my own.
+This is the first time I put something I wrote here (at least officially, and
+even though almost every arrangement I create is in some way a composition of
+mine).  This piece was born in… very unusual circumstances, to say the least.  I
+won't go into much detail — it's for the best, trust me —, but I will say as a
+bit of context that it's a short piece meant as an introductory soundtrack for a
+fictional story.  It's named <i>incomunicabilità</i> (it was baptized by an
+external entity, not by me).
+]],
+    par [[
+Lastly, as a result of the aforementioned activities which have been keeping me
+busy, everything has gotten better, except for the playing, so enjoy this new
+high-quality overhead camera setup.
+]],
+    blockquote(lines {
+        par [[
+“All my life I've been waiting for such a one as you, I knew someone like that
+would come and forgive me.  I believed that someone would love me, a dirty
+woman, not only for my shame…!”.
+]],
+        par [[
+“What did I do for you?” Alyosha answered with a tender smile, and he bent down
+to her and gently took her hands.  “I just gave you an onion, one little onion,
+that's all, that's all…!”.
+]],
+        quote_footer("Fyodor Dostoevsky", "The brothers Karamazov", "III.7.3"),
+    }),
+}
+
+return {
+    title = "Incomunicabilità",
+    author = "Bruno Barcarol Guimarães",
+    timestamp = { 1775764798, "2026-04-09T19:59:58" },
+    duration = "0:25",
+    poster = "0:06",
+    scores = {
+        link {
+            href = "/files/music/incomunicabilità.pdf",
+            content = "score",
+            target = "_blank",
+        },
+        link {
+            href = "/files/music/incomunicabilità.mscz",
+            content = "source",
+            target = "_blank",
+        },
+    },
+    tags = {"piano", "short", "original"},
+    links = {
+        facebook = "bruno.barcarolguimaraes/posts/pfbid0Ss6NxLyTtFp672DejXcaDM1UzEW3S6KvAABkuJ9Q6RmqpahynpMABua4bctXxW6ql",
+        instagram = "DW7POofjFXe",
+        youtube = "b8EYIgEtTic",
+    },
+    description = description,
+    content = content,
+}
