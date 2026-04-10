255208b4d7eaf7046ca87892362113b0948337a6
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2026-04-10 10:46:15 +0200

    music: as time goes by

---
+++ src/music/data/as_time_goes_by.lua
@@ -0,0 +1,66 @@
+local description <const> = [[<i>Each of us has a destiny. For good, or for evil</i>. Here's a short version of something that's been on my mind for the past few weeks.]]
+
+local content <const> = lines {
+    par [[
+Yesterday was one of those recording sessions where the very first try of each
+piece comes out almost perfectly, then you try again and again only for it to
+get worse each time (I'm sure every musician can relate).
+]],
+    par [[
+Here's a short version of something that's been on my mind for the past few
+weeks.  This early-jazz style of stride piano with wide dynamic range and chords
+is what made me want to learn to play the instrument in the first place and, as
+life goes, here I am some fifteen years later finally having the faintest hope
+of attempting it, prompted by the magnificent version that features in a film I
+watched recently.
+]],
+    par [[
+Can you guess which one? (that's a rhetorical question, I don't care, but you
+can “let me know in the comments below” if you'd like)
+]],
+    par [[
+Hint #1: it's the greatest film of all time.
+]],
+    par [[
+Hint #2: even though you can't see her, Ingrid Bergman sat just out of view
+saying “play it, Sam” with a longing look on her face and humming the melody.
+]],
+    blockquote(lines {
+        par [[
+Each of us has a destiny. For good, or for evil.
+]],
+        par [[
+I wonder if you know that you're trying to escape from yourself… and that you'll
+never succeed.
+]],
+    }),
+}
+
+return {
+    id = "as-time-goes-by",
+    title = "As time goes by",
+    author = "Herman Hupfeld",
+    timestamp = { 1775806426, "2026-04-10T07:33:46" },
+    duration = "0:31",
+    poster = "0:21.7",
+    scores = {
+        link {
+            href = "/files/music/as_time_goes_by.pdf",
+            content = "score",
+            target = "_blank",
+        },
+        link {
+            href = "/files/music/as_time_goes_by.mscz",
+            content = "source",
+            target = "_blank",
+        },
+    },
+    tags = {"piano", "short"},
+    links = {
+        facebook = "reel/2109440246500116",
+        instagram = "DW9ph1JDEsN",
+        youtube = "I9MQZicELlY",
+    },
+    description = description,
+    content = content,
+}
