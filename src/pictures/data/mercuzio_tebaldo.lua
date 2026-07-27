9e26080744d78114db56af8bc7ea26aa8176c720
Bruno Barcarol Guimarães <bbguimaraes@bbguimaraes.com>
2026-07-27 12:25:53 +0200

    pictures: Mercuzio & Tebaldo

---
+++ src/pictures/data/mercuzio_tebaldo.lua
@@ -0,0 +1,175 @@
+local description <const> = [[Allow me to introduce my friends Mercuzio and Tebaldo.]]
+
+local content <const> = lines {
+    par [[
+Allow me to introduce my friends: Mercuzio and Tebaldo (<i>a plague o' both your
+houses</i> if you don't recognize those names).  Tebaldo was the first to
+discover our beautiful balcony, about two years ago; Mercuzio came a couple of
+months later, and both have since been regular visitors, as well as regular
+depositors of nuts.
+]],
+    par [[
+So you can identify them: Mercuzio is almost all grey, and a bit more rotund.
+He's the less suspicious of the two, and will eat almonds straight from your
+hand.  Tebaldo has much more red squirrel in him, especially on the paws, is
+leaner, more athletic, and more suspicious.  He prefers to be safe and stores
+some of his possessions in his hideout under one of the chimneys of <i
+lang="it">palazzo Chiablese</i>.
+]],
+    par [[
+It's a mystery where exactly they come from, but they're likely one of the many
+furry inhabitants that densely populate <i lang="it">Giardini Reali</i>.  They
+always come to our courtyard via the roofs, through an impressive series of
+acrobatics, including a final jump, many times the length of their bodies, from
+a windowsill to the guardrail of our balcony over a three-floor gap, sometimes
+while carrying food (Mercuzio once got too proud and tried to steal away with a
+stick of butter almost his size that he had stolen from our kitchen, and of
+course failed).
+]],
+    par [[
+These pictures and videos span the two years of their visits — and vastly
+different weather, lighting conditions, and photographic equipment.  It's also
+funny to watch the progressive takeover of greenery over the balcony, of which I
+plead guilty.
+]],
+}
+
+local citation <const> = lines {
+    par [[
+Wisely and slow; they stumble that run fast
+]],
+    quote_footer("William Shakespeare", "Romeo &amp; Juliet", "2.2"),
+}
+
+local images <const> = {{
+    path = "mercuzio_tebaldo/IMG_20260617_070526.jpg",
+}, {
+    path = "mercuzio_tebaldo/VID_20240914_131145.mp4",
+    text = [[
+First distant sighting on the roof of <i lang="it">palazzo Chiablese</i>.
+]],
+}, {
+    path = "mercuzio_tebaldo/IMG_20241002_134334.jpg",
+    text = [[
+Enter Tebaldo.
+]],
+}, {
+    path = "mercuzio_tebaldo/VID_20241002_134340.mp4",
+}, {
+    path = "mercuzio_tebaldo/VID_20241002_134544.mp4",
+}, {
+    path = "mercuzio_tebaldo/VID_20241013_082632.mp4",
+}, {
+    path = "mercuzio_tebaldo/IMG_20241014_091106.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20241213_114317.jpg",
+    text = [[
+Enter Mercuzio.
+]],
+}, {
+    path = "mercuzio_tebaldo/IMG_20241213_114316.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20241213_114315.jpg",
+}, {
+    path = "mercuzio_tebaldo/VID_20241228_113224.mp4",
+    poster = "0:10",
+}, {
+    path = "mercuzio_tebaldo/IMG_20241228_112305.jpg",
+    text = [[
+Mercuzio in one of his many <i lang="it">pausa pranzo</i>.
+]],
+}, {
+    path = "mercuzio_tebaldo/IMG_20250308_072650.jpg",
+    crop = "33%x+20%",
+    text = [[
+Over time they've become an integral part of the scenery.
+]],
+}, {
+    path = "mercuzio_tebaldo/IMG_20250826_081638.jpg",
+}, {
+    path = "mercuzio_tebaldo/VID_20250410_072106.mp4",
+    poster = "0:02",
+}, {
+    path = "mercuzio_tebaldo/IMG_20250826_081130.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20250826_082038.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20251123_125700.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_064319.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_064355.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_064509.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_064912.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_064915.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_065537.jpg",
+    text = [[
+Tebaldo's deposit.
+]],
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_065539.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_065547.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_065616.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_070302.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_070610.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_070731.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260617_070755.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260701_174946.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260701_174936.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260701_174837.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260701_175018.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260701_174750.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20260701_175124.jpg",
+    text = [[
+Tebaldo preparing for the daring jump.
+]],
+}, {
+    path = "mercuzio_tebaldo/IMG_20250919_145805.jpg",
+    crop = "50%x100%+250!",
+}, {
+    path = "mercuzio_tebaldo/IMG_20250919_145833.jpg",
+    crop = "50%x100%+40!",
+}, {
+    path = "mercuzio_tebaldo/IMG_20250919_145852.jpg",
+    crop = "50%x100%+250!",
+}, {
+    path = "mercuzio_tebaldo/IMG_20250919_145904.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20250923_092308.jpg",
+    crop = "20%x80%+225-700!",
+}, {
+    path = "mercuzio_tebaldo/IMG_20250923_092141.jpg",
+    crop = "20%x80%+275-700!",
+}, {
+    path = "mercuzio_tebaldo/IMG_20250923_092142.jpg",
+    crop = "20%x80%+0-700!",
+}, {
+    path = "mercuzio_tebaldo/IMG_20250923_092217.jpg",
+}, {
+    path = "mercuzio_tebaldo/IMG_20250923_092223.jpg",
+}}
+
+return {
+    timestamp = { 1784913572, "2026-07-24" },
+    title = "Mercuzio &amp; Tebaldo",
+    description = description,
+    content = content,
+    citation = citation,
+    images = images,
+}
